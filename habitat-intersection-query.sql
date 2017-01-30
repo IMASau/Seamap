e4b6f8c683528cdb9abeba144bd29400cff55508
Mark Hepburn <Mark.Hepburn@gmail.com>
2017-01-30 17:28:47 +1100

    Turn split-geometries functionality into a function

--- habitat-intersection-query.sql
+++ habitat-intersection-query.sql
@@ -19,6 +19,27 @@
 -- union all
 -- select id, geom from lines;
 
+-- Given a geometry, return a table of geometries all of whom have STNumGeometries() == 1:
+IF OBJECT_ID('split_geom') IS NULL
+  EXEC('create function split_geom(@geom geometry) returns @split_geom table (geom geometry) as begin return; end');
+GO
+ALTER FUNCTION split_geom (@geom geometry)
+RETURNS @split_geom TABLE (geom geometry)
+AS
+BEGIN
+  DECLARE
+    @numgeoms int = @geom.STNumGeometries();
+  WITH Nums AS (
+    SELECT top (ISNULL(@numgeoms,0)) ROW_NUMBER() OVER(ORDER BY (SELECT NULL)) AS number
+    FROM master..spt_values t1 cross join master..spt_values t2 -- should provide plenty of size
+  )
+  --
+  INSERT INTO @split_geom (geom)
+  SELECT @geom.STGeometryN(n.number)
+  FROM (select @geom g) geomtable inner join Nums n on n.number <= @numgeoms;
+  RETURN;
+END;
+GO
 
 declare @line geometry = (select geom from Lines where id = 5);
 
