ee364d6eca40596a0878e15baf1332043694dbec
Mark Hepburn <Mark.Hepburn@gmail.com>
2017-01-31 17:25:09 +1100

    Add a css-transition-group class (just following the recipe)

--- frontend/src/cljs/imas_seamap/views.cljs
+++ frontend/src/cljs/imas_seamap/views.cljs
@@ -1,5 +1,9 @@
 (ns imas-seamap.views
-    (:require [re-frame.core :as re-frame]))
+  (:require [re-frame.core :as re-frame]
+            [reagent.core :as reagent]))
+
+(def css-transition-group
+  (reagent/adapt-react-class js/React.addons.CSSTransitionGroup))
 
 (defn main-panel []
   (let [name (re-frame/subscribe [:name])]
