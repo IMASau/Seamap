a608de4930053fadaf7a423175500decd9e71504
Mark Hepburn <Mark.Hepburn@gmail.com>
2017-02-01 11:33:51 +1100

    Add another marker, for testing
    Excellent; now we can for eg run
    (swap! re-frame.db/app-db update-in [:map :markers] pop)
    To remove one, exactly as you'd expect.
    
    (Caveat is of course we need to be aware of what's a dynamic property
    and what isn't; see React.Leaflet itself)

--- frontend/src/cljs/imas_seamap/db.cljs
+++ frontend/src/cljs/imas_seamap/db.cljs
@@ -4,4 +4,5 @@
   {:name "re-frame"
    :map {:pos [51.505 -0.09]
          :zoom 13
-         :markers [{:pos [51.505 -0.09] :title "One"}]}})
+         :markers [{:pos [51.505 -0.09] :title "One"}
+                   {:pos [51.507 -0.10] :title "Two"}]}})
