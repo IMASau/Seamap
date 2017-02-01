bf41ebdf4578b34c7f50f512b6af51ca2d432b2d
Mark Hepburn <Mark.Hepburn@gmail.com>
2017-02-01 11:19:22 +1100

    Port map component to type-2 so we can use destructuring

--- frontend/src/cljs/imas_seamap/views/map.cljs
+++ frontend/src/cljs/imas_seamap/views/map.cljs
@@ -12,15 +12,16 @@
 (defn map-component []
   ;; pos/zoom would normally come from the state, of course:
   (let [map-props (re-frame/subscribe [:map/props])]
-    [leaflet-map {:id "map" :center (:pos @map-props) :zoom (:zoom @map-props)
-                    ;; Event handlers must start with "on" and are hooked up automatically
-                    ;; (see http://leafletjs.com/reference.html#map-events)
-                    ;; Downside is the only info you get is the event type and target (probably the map):
-                    :ondrag #(println "dragging..." (-> (.-target %) .getCenter js->clj (select-keys ["lat" "lng"])))}
-     [tile-layer {:url "http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-                    :attribution "&copy; <a href=\"http://osm.org/copyright\">OpenStreetMap</a> contributors"}]
-     [marker {:position (:pos @map-props)}
-      [popup {:position (:pos @map-props)}
-       [:div.classname
-        [:b "Roar!"]
-        [:p "Testing testing, " [:i "one two three..."]]]]]]))
+    #(let [{:keys [pos zoom]} @map-props]
+       [leaflet-map {:id "map" :center pos :zoom zoom
+                     ;; Event handlers must start with "on" and are hooked up automatically
+                     ;; (see http://leafletjs.com/reference.html#map-events)
+                     ;; Downside is the only info you get is the event type and target (probably the map):
+                     :ondrag (fn [e] (println "dragging..." (-> (.-target e) .getCenter js->clj (select-keys ["lat" "lng"]))))}
+        [tile-layer {:url "http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+                     :attribution "&copy; <a href=\"http://osm.org/copyright\">OpenStreetMap</a> contributors"}]
+        [marker {:position pos}
+         [popup {:position pos}
+          [:div.classname
+           [:b "Roar!"]
+           [:p "Testing testing, " [:i "one two three..."]]]]]])))
