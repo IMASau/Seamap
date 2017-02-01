cd34fdfc521c254287ee4eb75902a12db1e913b4
Mark Hepburn <Mark.Hepburn@gmail.com>
2017-02-01 11:48:45 +1100

    Port a wms layer too, and use blue marble in the test-bed

--- frontend/src/cljs/imas_seamap/db.cljs
+++ frontend/src/cljs/imas_seamap/db.cljs
@@ -3,6 +3,6 @@
 (def default-db
   {:name "re-frame"
    :map {:pos [51.505 -0.09]
-         :zoom 13
+         :zoom 3
          :markers [{:pos [51.505 -0.09] :title "One"}
                    {:pos [51.507 -0.10] :title "Two"}]}})
--- frontend/src/cljs/imas_seamap/views/map.cljs
+++ frontend/src/cljs/imas_seamap/views/map.cljs
@@ -5,6 +5,7 @@
 
 
 (def tile-layer  (r/adapt-react-class js/ReactLeaflet.TileLayer))
+(def wms-layer   (r/adapt-react-class js/ReactLeaflet.WMSTileLayer))
 (def leaflet-map (r/adapt-react-class js/ReactLeaflet.Map))
 (def marker      (r/adapt-react-class js/ReactLeaflet.Marker))
 (def popup       (r/adapt-react-class js/ReactLeaflet.Popup))
@@ -14,8 +15,9 @@
   (let [map-props (re-frame/subscribe [:map/props])]
     #(let [{:keys [pos zoom markers]} @map-props]
        [leaflet-map {:id "map" :center pos :zoom zoom}
-        [tile-layer {:url "http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-                     :attribution "&copy; <a href=\"http://osm.org/copyright\">OpenStreetMap</a> contributors"}]
+        [wms-layer {:url "http://demo.opengeo.org/geoserver/ows?"
+                    :layers "nasa:bluemarble"
+                    :attribution "Made by Condense / Images by NASA"}]
         (for [{:keys [pos title]} markers]
           ^{:key (str pos)}
           [marker {:position pos}
