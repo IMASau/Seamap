1401b7f483d6ab42debaec343d9ecd4d9ada0df2
Mark Hepburn <Mark.Hepburn@gmail.com>
2017-02-01 11:35:39 +1100

    Get rid of noisy event handler, just to protect my terminal

--- frontend/src/cljs/imas_seamap/views/map.cljs
+++ frontend/src/cljs/imas_seamap/views/map.cljs
@@ -13,11 +13,7 @@
   ;; pos/zoom would normally come from the state, of course:
   (let [map-props (re-frame/subscribe [:map/props])]
     #(let [{:keys [pos zoom markers]} @map-props]
-       [leaflet-map {:id "map" :center pos :zoom zoom
-                     ;; Event handlers must start with "on" and are hooked up automatically
-                     ;; (see http://leafletjs.com/reference.html#map-events)
-                     ;; Downside is the only info you get is the event type and target (probably the map):
-                     :ondrag (fn [e] (println "dragging..." (-> (.-target e) .getCenter js->clj (select-keys ["lat" "lng"]))))}
+       [leaflet-map {:id "map" :center pos :zoom zoom}
         [tile-layer {:url "http://{s}.tile.osm.org/{z}/{x}/{y}.png"
                      :attribution "&copy; <a href=\"http://osm.org/copyright\">OpenStreetMap</a> contributors"}]
         (for [{:keys [pos title]} markers]
