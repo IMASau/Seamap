e5bd6029c23d884decf3ab25b257c5ab04ebbadd
Mark Hepburn <Mark.Hepburn@gmail.com>
2017-02-01 11:28:19 +1100

    Make markers a list property of the db

--- frontend/src/cljs/imas_seamap/db.cljs
+++ frontend/src/cljs/imas_seamap/db.cljs
@@ -2,4 +2,6 @@
 
 (def default-db
   {:name "re-frame"
-   :map {:pos [51.505 -0.09] :zoom 13}})
+   :map {:pos [51.505 -0.09]
+         :zoom 13
+         :markers [{:pos [51.505 -0.09]}]}})
--- frontend/src/cljs/imas_seamap/views/map.cljs
+++ frontend/src/cljs/imas_seamap/views/map.cljs
@@ -12,7 +12,7 @@
 (defn map-component []
   ;; pos/zoom would normally come from the state, of course:
   (let [map-props (re-frame/subscribe [:map/props])]
-    #(let [{:keys [pos zoom]} @map-props]
+    #(let [{:keys [pos zoom markers]} @map-props]
        [leaflet-map {:id "map" :center pos :zoom zoom
                      ;; Event handlers must start with "on" and are hooked up automatically
                      ;; (see http://leafletjs.com/reference.html#map-events)
@@ -20,8 +20,10 @@
                      :ondrag (fn [e] (println "dragging..." (-> (.-target e) .getCenter js->clj (select-keys ["lat" "lng"]))))}
         [tile-layer {:url "http://{s}.tile.osm.org/{z}/{x}/{y}.png"
                      :attribution "&copy; <a href=\"http://osm.org/copyright\">OpenStreetMap</a> contributors"}]
-        [marker {:position pos}
-         [popup {:position pos}
-          [:div.classname
-           [:b "Roar!"]
-           [:p "Testing testing, " [:i "one two three..."]]]]]])))
+        (for [{:keys [pos]} markers]
+          ^{:key (str pos)}
+          [marker {:position pos}
+           [popup {:position pos}
+            [:div.classname
+             [:b "Roar!"]
+             [:p "Testing testing, " [:i "one two three..."]]]]])])))
