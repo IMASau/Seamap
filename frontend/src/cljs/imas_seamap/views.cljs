86f7d172bb81cc025d93b1bc998e29fa292223ba
Mark Hepburn <Mark.Hepburn@gmail.com>
2017-02-02 12:46:34 +1100

    Clean up the old starter-view, and add basic layout (no css yet)

--- frontend/src/cljs/imas_seamap/views.cljs
+++ frontend/src/cljs/imas_seamap/views.cljs
@@ -1,11 +1,14 @@
 (ns imas-seamap.views
   (:require [re-frame.core :as re-frame]
-            [reagent.core :as reagent]))
+            [reagent.core :as reagent]
+            [imas-seamap.views.map :refer [map-component]]))
 
 (def css-transition-group
   (reagent/adapt-react-class js/React.addons.CSSTransitionGroup))
 
-(defn main-panel []
-  (let [name (re-frame/subscribe [:name])]
-    (fn []
-      [:div "Hello from " @name])))
+(defn layout-app []
+  [:div {:style {:height "100%" :width "100%"}}
+   [:div.sidebar]
+   [:div.main-wrapper
+    [map-component]
+    [:div#footer]]])
