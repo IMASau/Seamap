e83e4b7383214ad25924fdcbb61556dcec2c3754
Mark Hepburn <Mark.Hepburn@gmail.com>
2017-02-06 10:22:04 +1100

    Add standard debug interceptor to event handlers. Closes SM-7

--- frontend/src/cljs/imas_seamap/core.cljs
+++ frontend/src/cljs/imas_seamap/core.cljs
@@ -1,6 +1,7 @@
 (ns imas-seamap.core
   (:require [reagent.core :as reagent]
             [re-frame.core :as re-frame]
+            [re-frame.std-interceptors :refer [debug]]
             [re-frisk.core :refer [enable-re-frisk!]]
             [imas-seamap.events :as events]
             [imas-seamap.subs :as subs]
@@ -12,11 +13,17 @@
   {:subs   {:map/props     subs/map-props}
    :events {:initialise-db events/-initialise-db}})
 
+(def standard-interceptors
+  [(when ^boolean goog.DEBUG debug)])
+
 (defn register-handlers! [{:keys [subs events]}]
   (doseq [[sym handler] subs]
     (re-frame/reg-sub sym handler))
   (doseq [[sym handler] events]
-    (re-frame/reg-event-db sym handler)))
+    (re-frame/reg-event-db
+     sym
+     standard-interceptors
+     handler)))
 
 (defn dev-setup []
   (when config/debug?
