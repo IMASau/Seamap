8a2f7fdc1f7d7f3a17386844a63a6b73af7113f8
Mark Hepburn <Mark.Hepburn@gmail.com>
2017-02-06 14:39:25 +1100

    Initial ajax implementation -- just GET for now
    This may need to be fleshed out; for now it takes a URL and keys for
    the success and error handlers (default implementations provided), and
    other keys can be overridden as necessary

--- frontend/src/cljs/imas_seamap/events.cljs
+++ frontend/src/cljs/imas_seamap/events.cljs
@@ -1,7 +1,30 @@
 (ns imas-seamap.events
-    (:require [re-frame.core :as re-frame]
-              [imas-seamap.db :as db]))
+  (:require [ajax.core :as ajax]
+            [imas-seamap.db :as db]
+            [re-frame.core :as re-frame]
+            [debux.cs.core :refer [dbg]]))
 
 ;;; TODO: maybe pull in extra config inject as page config, but that
 ;;; may not be feasible with a wordpress host
 (defn -initialise-db [_ _] db/default-db)
+
+
+(defn ajax [db [_ url {:keys [handler err-handler override-opts]
+                       :or   {handler     :ajax/default-success-handler
+                              err-handler :ajax/default-err-handler}
+                       :as opts}]]
+  (ajax/GET url
+            (merge
+             {:handler #(re-frame/dispatch [handler %])
+              :error-handler #(re-frame/dispatch [err-handler %])
+              :response-format :json :keywords? true}
+             override-opts))
+  db)
+
+(defn default-success-handler [db [_ response]]
+  (js/console.info "Received AJAX response" response)
+  db)
+
+(defn default-err-handler [db [_ response]]
+  (js/console.info "AJAX error response" response)
+  db)
