8e49488926ddf663c3ce1742c8d03ec4ff320178
Mark Hepburn <Mark.Hepburn@gmail.com>
2017-02-06 14:33:10 +1100

    Comment about incorporating in-page config (Ref SM-8)

--- frontend/src/cljs/imas_seamap/events.cljs
+++ frontend/src/cljs/imas_seamap/events.cljs
@@ -2,4 +2,6 @@
     (:require [re-frame.core :as re-frame]
               [imas-seamap.db :as db]))
 
+;;; TODO: maybe pull in extra config inject as page config, but that
+;;; may not be feasible with a wordpress host
 (defn -initialise-db [_ _] db/default-db)
