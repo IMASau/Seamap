59afd3b8c351318d02a20baf3a550c76e0ac2408
Mark Hepburn <Mark.Hepburn@gmail.com>
2017-02-10 13:44:35 +1100

    Bump to latest clojurescript version

--- frontend/project.clj
+++ frontend/project.clj
@@ -1,6 +1,6 @@
 (defproject imas-seamap "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.9.0-alpha10"]
-                 [org.clojure/clojurescript "1.9.229"]
+                 [org.clojure/clojurescript "1.9.456"]
                  [cljs-ajax "0.5.5"]
                  [org.clojure/test.check "0.9.0"]
                  [figwheel-sidecar "0.5.4-7"]
