980cde26fda99a815af8a8ae20c94a404c0810c1
Mark Hepburn <Mark.Hepburn@gmail.com>
2017-02-10 17:34:51 +1100

    Wire up the map; separate layer draws transect when drawing finished

--- frontend/src/cljs/imas_seamap/views/map.cljs
+++ frontend/src/cljs/imas_seamap/views/map.cljs
@@ -5,6 +5,7 @@
 
 (def tile-layer    (r/adapt-react-class js/ReactLeaflet.TileLayer))
 (def wms-layer     (r/adapt-react-class js/ReactLeaflet.WMSTileLayer))
+(def geojson-layer (r/adapt-react-class js/ReactLeaflet.GeoJSON))
 (def leaflet-map   (r/adapt-react-class js/ReactLeaflet.Map))
 (def marker        (r/adapt-react-class js/ReactLeaflet.Marker))
 (def popup         (r/adapt-react-class js/ReactLeaflet.Popup))
@@ -13,6 +14,7 @@
 
 (defn map-component []
   (let [{:keys [pos zoom markers controls layer-idx]} @(re-frame/subscribe [:map/props])
+        {:keys [drawing? query]} @(re-frame/subscribe [:transect/info])
         wl [wms-layer {:url "http://demo.opengeo.org/geoserver/ows?"
                        :layers "nasa:bluemarble"
                        :attribution "Made by Condense / Images by NASA"}]
@@ -21,16 +23,19 @@
     [leaflet-map {:id "map" :center pos :zoom zoom}
      ;; Just hacking around, to test swapping layers in and out:
      (if (odd? layer-idx) wl tl)
-     (when ^boolean (:transect controls)
+     (when query
+       [geojson-layer {:data query}])
+     (when drawing?
        [feature-group
         [edit-control {:draw {:rectangle false
                               :circle    false
                               :marker    false
                               :polygon   false
                               :polyline  {:allowIntersection false}}
-                       :on-mounted #(do (.. % -_toolbars -draw -_modes -polyline -handler enable)
-                                        (.. % -_map  (on "draw:drawstop" (fn [e] (js/console.warn "stopped" e)))))
-                       :on-created #(js/console.warn "Got me a drawing!" % (.. % -layer toGeoJSON))}]])
+                       :on-mounted (fn [e]
+                                     (.. e -_toolbars -draw -_modes -polyline -handler enable)
+                                     (.. e -_map  (once "draw:drawstop" #(re-frame/dispatch [:transect.draw/disable]))))
+                       :on-created #(re-frame/dispatch [:transect/query (-> % .-layer .toGeoJSON)])}]])
      (for [{:keys [pos title]} markers]
        ^{:key (str pos)}
        [marker {:position pos}
