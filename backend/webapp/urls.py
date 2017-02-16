ce70832e3167da1c135d6a9539ad12af2ac2b584
Mark Hepburn <Mark.Hepburn@gmail.com>
2017-02-16 11:14:59 +1100

    Only enable admin if in DEBUG mode. Closes SM-20

--- backend/webapp/urls.py
+++ backend/webapp/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 from rest_framework.routers import DefaultRouter
@@ -11,7 +12,11 @@ router.register(r'layers', LayerViewset)
 router.register(r'habitat', HabitatViewSet, 'Habitat')
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
     url(r'^', include(router.urls)),
-    url(r'^api-auth.', include('rest_framework.urls', namespace='rest_framework'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        url(r'^admin/', admin.site.urls),
+        url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
+    ]
