b8cdbe0e19187a508b149c44bd775ed537e0468f
Mark Hepburn <Mark.Hepburn@gmail.com>
2017-02-17 14:30:55 +1100

    Test-data had incorrect server url

--- frontend/src/cljs/imas_seamap/map/events.cljs
+++ frontend/src/cljs/imas_seamap/map/events.cljs
@@ -4,9 +4,9 @@
 
 
 (def ^:private test-layer-data
-  [{:name "National" :server_url "http://geoserver.imas.utas.edu.au/geoserver/seamap/wms?" :layer_name "seamap:SeamapAus_NAT_CoastalWaterways_geomorphic" :category "habitat" :bounding_box "" :metadata_url "" :description "" :zoom_info "" :server_type "geoserver" :legend_url "" :date_start "" :date_end ""}
-   {:name "NSW" :server_url "http://geoserver.imas.utas.edu.au/geoserver/seamap/wms?" :layer_name "seamap:SeamapAus_NSW_ocean_ecosystems_2002" :category "habitat" :bounding_box "" :metadata_url "" :description "" :zoom_info "" :server_type "geoserver" :legend_url "" :date_start "" :date_end ""}
-   {:name "Tasmania" :server_url "http://geoserver.imas.utas.edu.au/geoserver/seamap/wms?" :layer_name "cite:SEAMAP_habitats_Geo" :category "habitat" :bounding_box "" :metadata_url "" :description "" :zoom_info "" :server_type "geoserver" :legend_url "" :date_start "" :date_end ""}
+  [{:name "National" :server_url "http://geoserver.imas.utas.edu.au/geoserver/wms?" :layer_name "seamap:SeamapAus_NAT_CoastalWaterways_geomorphic" :category "habitat" :bounding_box "" :metadata_url "" :description "" :zoom_info "" :server_type "geoserver" :legend_url "" :date_start "" :date_end ""}
+   {:name "NSW" :server_url "http://geoserver.imas.utas.edu.au/geoserver/wms?" :layer_name "seamap:SeamapAus_NSW_ocean_ecosystems_2002" :category "habitat" :bounding_box "" :metadata_url "" :description "" :zoom_info "" :server_type "geoserver" :legend_url "" :date_start "" :date_end ""}
+   {:name "Tasmania" :server_url "http://geoserver.imas.utas.edu.au/geoserver/wms?" :layer_name "cite:SEAMAP_habitats_Geo" :category "habitat" :bounding_box "" :metadata_url "" :description "" :zoom_info "" :server_type "geoserver" :legend_url "" :date_start "" :date_end ""}
    #_{:name "" :server_url "" :layer_name "" :category "bathymetry" :bounding_box "" :metadata_url "" :description "" :zoom_info "" :server_type "" :legend_url "" :date_start "" :date_end ""}
    #_{:name "" :server_url "" :layer_name "" :category "bathymetry" :bounding_box "" :metadata_url "" :description "" :zoom_info "" :server_type "" :legend_url "" :date_start "" :date_end ""}
    #_{:name "" :server_url "" :layer_name "" :category "imagery" :bounding_box "" :metadata_url "" :description "" :zoom_info "" :server_type "" :legend_url "" :date_start "" :date_end ""}
