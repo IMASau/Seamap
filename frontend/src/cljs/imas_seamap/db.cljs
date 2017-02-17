f5a0dadd4277e6f2e8ed6dd851a6e367a906f34d
Mark Hepburn <Mark.Hepburn@gmail.com>
2017-02-17 12:38:04 +1100

    Add spec for layer entry in app-db

--- frontend/src/cljs/imas_seamap/db.cljs
+++ frontend/src/cljs/imas_seamap/db.cljs
@@ -3,6 +3,7 @@
 (def default-db
   {:map {:pos [-23.116667 132.133333]
          :zoom 4
+         :layers []
          :controls {:transect false}}
    :transect nil
    :config {:catalogue-url "http://localhost:8000/api/layers/"}})
