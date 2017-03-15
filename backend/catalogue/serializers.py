b828ca36ee34e7d0e9b2ff6307c5d343608f3abf
Mark Hepburn <Mark.Hepburn@gmail.com>
2017-03-15 15:06:25 +1100

    Layer serialiser: ensure that category/type names come through
    (not pks).  Not sure if this is the best way of doing it?  Feels like
    I'm missing a trick

--- backend/catalogue/serializers.py
+++ backend/catalogue/serializers.py
@@ -2,10 +2,16 @@ from rest_framework import serializers
 from catalogue.models import Layer
 
 
-
 class LayerSerializer(serializers.ModelSerializer):
+    category = serializers.SerializerMethodField()
+    server_type = serializers.SerializerMethodField()
+
+    def get_category(self, obj):
+        return obj.category.name
+
+    def get_server_type(self, obj):
+        return obj.server_type.name
+
     class Meta:
         model = Layer
-        fields = ('name', 'url', 'category', 'bounding_box', 
-                'metadata_url', 'description', 'zoom_info', 'server_type', 
-                'legend_url', 'date_start', 'date_end')
+        fields = '__all__'
