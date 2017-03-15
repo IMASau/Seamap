81c7153d35bfef0ffdc4fe3516898e2e3e913a9f
Mark Hepburn <Mark.Hepburn@gmail.com>
2017-03-15 13:48:22 +1100

    Use floating-point validator not integer

--- backend/catalogue/models.py
+++ backend/catalogue/models.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
-from django.core.validators import validate_comma_separated_integer_list
+import re
+
+from django.core.validators import RegexValidator
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 
@@ -20,16 +22,23 @@ class ServerType(models.Model):
         return self.name
 
 
+comma_separated_float_list_re = re.compile(r'^[-\.\d,]+\Z')  # no, not very sophisticated
+validate_comma_separated_float_list = RegexValidator(
+    comma_separated_float_list_re,
+    'Must be a comma-separated list of floating-point numbers',
+    'invalid'
+)
+
 @python_2_unicode_compatible
 class Layer(models.Model):
     name = models.CharField(max_length = 200)
     server_url = models.URLField(max_length = 200)
     layer_name = models.CharField(max_length = 200)
     category = models.ForeignKey(Category)
-    bounding_box = models.CharField(validators=[validate_comma_separated_integer_list], max_length = 200)
+    bounding_box = models.CharField(validators=[validate_comma_separated_float_list], max_length = 200)
     metadata_url = models.URLField(max_length = 200)
     description = models.CharField(max_length = 500)
-    zoom_info = models.CharField(validators=[validate_comma_separated_integer_list], max_length = 200)
+    zoom_info = models.CharField(validators=[validate_comma_separated_float_list], max_length = 200)
     detail_resolution = models.BooleanField()
     server_type = models.ForeignKey(ServerType)
     legend_url = models.URLField(max_length=200)
