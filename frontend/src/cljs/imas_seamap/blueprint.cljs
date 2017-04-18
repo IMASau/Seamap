a16b0c0665cad391e39414829101ffbd898d34f7
Mark Hepburn <Mark.Hepburn@gmail.com>
2017-04-18 11:25:55 +1000

    Add tabs and tree components from blueprint

--- frontend/src/cljs/imas_seamap/blueprint.cljs
+++ frontend/src/cljs/imas_seamap/blueprint.cljs
@@ -16,3 +16,9 @@
 (def overlay (reagent/adapt-react-class js/Blueprint.Overlay))
 
 (def spinner (reagent/adapt-react-class js/Blueprint.Spinner))
+
+(def tabs (reagent/adapt-react-class js/Blueprint.Tabs2))
+
+(def tab (reagent/adapt-react-class js/Blueprint.Tab2))
+
+(def tree (reagent/adapt-react-class js/Blueprint.Tree))
