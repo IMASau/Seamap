2d762374645d3c7330919ba5bdfa71f1130e1803
Mark Hepburn <Mark.Hepburn@gmail.com>
2017-04-18 13:51:23 +1000

    Add data_classification field (name attribute of) to layer serialiser

--- backend/catalogue/serializers.py
+++ backend/catalogue/serializers.py
@@ -5,12 +5,16 @@ from catalogue.models import Layer
 class LayerSerializer(serializers.ModelSerializer):
     category = serializers.SerializerMethodField()
     server_type = serializers.SerializerMethodField()
+    data_classification = serializers.SerializerMethodField()
 
     def get_category(self, obj):
         return obj.category.name
 
     def get_server_type(self, obj):
         return obj.server_type.name
+
+    def get_data_classification(self, obj):
+        return getattr(obj.data_classification, 'name', None)
 
     class Meta:
         model = Layer
