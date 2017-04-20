7d1cf761d9d0fc0f10d201b1968f9b0f3a39c4a8
Mark Hepburn <Mark.Hepburn@gmail.com>
2017-04-20 11:26:50 +1000

    project.clj tweak for figwheel: add cljs to :source-paths
    That's an hour I'm not getting back: figwheel uses the top-level
    project definition for clojure compilation, so a clj file for macro
    definition that I had in the src/cljs directory wasn't being
    compiled (although it *is* compiled by plain lein-cljsbuild).  Add
    src/cljs to source-paths and everyone is happy.  (it's arguably more
    correct to move the macro clj file to src/clj, but I'm happy with it
    where it is)

--- frontend/project.clj
+++ frontend/project.clj
@@ -22,7 +22,7 @@
 
   :min-lein-version "2.5.3"
 
-  :source-paths ["src/clj"]
+  :source-paths ["src/clj" "src/cljs"]
 
   :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
 
