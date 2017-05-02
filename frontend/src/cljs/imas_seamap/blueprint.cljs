d4f97be32d6816d79392026d0fd2302a91986ef0
Mark Hepburn <Mark.Hepburn@gmail.com>
2017-05-02 15:04:29 +1000

    Minore change: convert oget+js/window to gget in blueprint adapters

--- frontend/src/cljs/imas_seamap/blueprint.cljs
+++ frontend/src/cljs/imas_seamap/blueprint.cljs
@@ -1,27 +1,27 @@
 (ns imas-seamap.blueprint
   "A collection of components adapted from blueprintjs"
-  (:require [oops.core :refer [oget]]
+  (:require [oops.core :refer [gget]]
             [reagent.core :as reagent]))
 
 
-(def button          (reagent/adapt-react-class (oget js/window "Blueprint.Button")))
+(def button          (reagent/adapt-react-class (gget "Blueprint.Button")))
 
-(def clipped-text    (reagent/adapt-react-class (oget js/window "Blueprint.Text")))
+(def clipped-text    (reagent/adapt-react-class (gget "Blueprint.Text")))
 
-(def collapse        (reagent/adapt-react-class (oget js/window "Blueprint.Collapse")))
+(def collapse        (reagent/adapt-react-class (gget "Blueprint.Collapse")))
 
-(def dialogue        (reagent/adapt-react-class (oget js/window "Blueprint.Dialog")))
+(def dialogue        (reagent/adapt-react-class (gget "Blueprint.Dialog")))
 
-(def non-ideal-state (reagent/adapt-react-class (oget js/window "Blueprint.NonIdealState")))
+(def non-ideal-state (reagent/adapt-react-class (gget "Blueprint.NonIdealState")))
 
-(def overlay         (reagent/adapt-react-class (oget js/window "Blueprint.Overlay")))
+(def overlay         (reagent/adapt-react-class (gget "Blueprint.Overlay")))
 
-(def spinner         (reagent/adapt-react-class (oget js/window "Blueprint.Spinner")))
+(def spinner         (reagent/adapt-react-class (gget "Blueprint.Spinner")))
 
-(def tabs            (reagent/adapt-react-class (oget js/window "Blueprint.Tabs2")))
+(def tabs            (reagent/adapt-react-class (gget "Blueprint.Tabs2")))
 
-(def tab             (reagent/adapt-react-class (oget js/window "Blueprint.Tab2")))
+(def tab             (reagent/adapt-react-class (gget "Blueprint.Tab2")))
 
-(def tooltip         (reagent/adapt-react-class (oget js/window "Blueprint.Tooltip")))
+(def tooltip         (reagent/adapt-react-class (gget "Blueprint.Tooltip")))
 
-(def tree            (reagent/adapt-react-class (oget js/window "Blueprint.Tree")))
+(def tree            (reagent/adapt-react-class (gget "Blueprint.Tree")))
