ec3143f4aee194b9c5f523d237f315dc91b84819
Mark Hepburn <Mark.Hepburn@gmail.com>
2017-05-10 14:24:30 +1000

    Transect model: tweak max digits and decimal places
    Now that we're using MGA (epsg3112) on the server, we need to adjust
    the numeric storage requirements.  The units are meters, so even 2
    decimal places is probably overkill, and can possibly be elided.  Most
    integer components are 7 digits, so call it 10 total to be safe.

--- backend/habitat/models.py
+++ backend/habitat/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 class Transect(models.Model):
     name = models.CharField(max_length = 200)
-    percentage = models.DecimalField(max_digits=10, decimal_places=5)
-    startx = models.DecimalField(max_digits=10, decimal_places=5)
-    starty = models.DecimalField(max_digits=10, decimal_places=5)
-    endx = models.DecimalField(max_digits=10, decimal_places=5)
-    endy = models.DecimalField(max_digits=10, decimal_places=5)
+    percentage = models.DecimalField(max_digits=10, decimal_places=2)
+    startx = models.DecimalField(max_digits=10, decimal_places=2)
+    starty = models.DecimalField(max_digits=10, decimal_places=2)
+    endx = models.DecimalField(max_digits=10, decimal_places=2)
+    endy = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return '{}: {} ({},{}) -> ({},{})'.format(self.name, self.percentage, self.startx, self.starty, self.endx, self.endy)
