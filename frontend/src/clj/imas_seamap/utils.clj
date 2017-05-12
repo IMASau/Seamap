48470a633104b905e7955fb125716d23184ee924
Mark Hepburn <Mark.Hepburn@gmail.com>
2017-05-12 13:33:27 +1000

    Utility to extract SM_HAB_CLS->colour mappings from SLDs. Ref SM-48
    The final version before this ticket can be closed is confirmation.
    Looks like the sld:Title attribute isn't always the same as the
    SM_HAB_CLS value, which is actually matched via a property-is-equal or
    occasionally is-like match (to circumvent character encoding issues I
    think)
    
    So we may make a couple of improvements:
    * Extract the title as well, and also map sm_hab_cls->title, so our
      tooltip matches the legend
    * We can get a list of distinct SM_HAB_CLS values, so we could
      manually expand the wildcards against those values and form the
      mapping that way.

---
+++ frontend/src/clj/imas_seamap/utils.clj
@@ -0,0 +1,41 @@
+(ns imas-seamap.utils
+  "We need to automatically extract colours mapped against different
+  SM_HAB_CLS values for the plot.  We do this by parsing the geoserver
+  SLD (styled layer descriptor) files.  Designed to be used
+  interactively from the REPL; not part of the client-application
+  itself."
+  (:require [clojure.data.xml :as xml]
+            [clojure.data.zip.xml :as zx]
+            [clojure.java.io :as io]
+            [clojure.zip :as zip]))
+
+
+(xml/alias-uri 'sld "http://www.opengis.net/sld")
+(xml/alias-uri 'ogc "http://www.opengis.net/ogc")
+
+(defn- rule->mapping [rule]
+  [[(zx/xml1-> rule ::sld/Title zx/text)
+    (zx/xml1-> rule ::sld/PolygonSymbolizer ::sld/Fill ::sld/CssParameter (zx/attr= :name "fill") zx/text)]])
+
+(defn- sld->colour-map [zipped-sld]
+  (let [rules (zx/xml-> zipped-sld
+                        ::sld/StyledLayerDescriptor
+                        ::sld/NamedLayer
+                        ::sld/UserStyle
+                        ::sld/FeatureTypeStyle
+                        ::sld/Rule
+                        rule->mapping)]
+    (into {} rules)))
+
+(defn colour-scheme-from-sld
+  "We need a colour scheme, mapping habitat classifications to
+  colours. This also needs to match the same scheme used in the
+  geoserver layers.  We achieve this by this utility, which parses a
+  geoserver .sld file and extracting the colour mapping from it."
+  [filename]
+  (-> filename
+      io/resource
+      slurp
+      xml/parse-str
+      zip/xml-zip
+      sld->colour-map))
