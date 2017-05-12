abd563a26c36b8329b20653b26589b73309bf070
Mark Hepburn <Mark.Hepburn@gmail.com>
2017-05-12 17:59:31 +1000

    Mostly-working functions to map SM_HAB_CLS -> colour / legend
    (needs a big tidy-up)

--- frontend/src/clj/imas_seamap/munging.clj
+++ frontend/src/clj/imas_seamap/munging.clj
@@ -14,11 +14,48 @@
 (xml/alias-uri 'sld "http://www.opengis.net/sld")
 (xml/alias-uri 'ogc "http://www.opengis.net/ogc")
 
-(defn- rule->mapping [rule]
+(defn rule->mapping [rule]
   [[(zx/xml1-> rule ::sld/Title zx/text)
     (zx/xml1-> rule ::sld/PolygonSymbolizer ::sld/Fill ::sld/CssParameter (zx/attr= :name "fill") zx/text)]])
 
-(defn- sld->colour-map [zipped-sld]
+(defn rule->info [rule]
+  (let [legend   (zx/xml1-> rule ::sld/Title zx/text)
+        literal  (zx/xml1-> rule ::ogc/Filter ::ogc/PropertyIsEqualTo ::ogc/Literal zx/text)
+        wildcard (zx/xml1-> rule ::ogc/Filter ::ogc/PropertyIsLike ::ogc/Literal zx/text)
+        key      (or literal wildcard)
+        colour   (zx/xml1-> rule ::sld/PolygonSymbolizer ::sld/Fill ::sld/CssParameter (zx/attr= :name "fill") zx/text)]
+    (when-not legend (println "****" legend " Complex expression"))
+    (when-not colour (println "****" legend " No colour (assumed background image)"))
+    [key legend colour]))
+
+(defn sld->rules [zipped-sld]
+  (zx/xml-> zipped-sld
+            ::sld/StyledLayerDescriptor
+            ::sld/NamedLayer
+            ::sld/UserStyle
+            ::sld/FeatureTypeStyle
+            ::sld/Rule))
+
+(defn info->legend-map [habitat-classes [key legend _colour :as info]]
+  (into {} (for [habitat-entry (glob key habitat-classes)]
+             [habitat-entry legend])))
+
+(defn info->colour-map [habitat-classes [key _legend colour :as info]]
+  (into {} (for [habitat-entry (glob key habitat-classes)]
+             [habitat-entry colour])))
+
+(defn rules->mappings
+  "Returns a pair of maps; the first from SM_HAB_CLS values to
+  colours, and the second to legends."
+  [rules habitat-classes]
+  (let [rule-info      (map rule->info rules)
+        colour-mapping (apply merge
+                              (map (partial info->colour-map habitat-classes) rule-info))
+        legend-mapping (apply merge
+                              (map (partial info->legend-map habitat-classes) rule-info))]
+    [colour-mapping legend-mapping]))
+
+(defn sld->colour-map [zipped-sld]
   (let [rules (zx/xml-> zipped-sld
                         ::sld/StyledLayerDescriptor
                         ::sld/NamedLayer
@@ -43,6 +80,6 @@
 
 ;;; TODO:
 ;;; * Extract all of sld:Title, ogc:Literal, and the fill colour (keyed by literal?)
-;;; * Warn about nodes that have no fill (it's probably an image), or a complex-expression (ogc:Or, ogc:And; 16 items)
+;;; * Warn about nodes that have no fill (it's probably an image), or a complex-expression (ogc:Or, ogc:And; 16 items, + 1 <Or>)
 ;;; * expand out globs against the habitats list
 ;;; * Another lookup, SM_HAB_CLS -> Title (matches legend)
