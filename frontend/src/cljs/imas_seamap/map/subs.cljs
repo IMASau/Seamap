60fbb603f90373abc61c5217ca75de0d3369d78c
Mark Hepburn <Mark.Hepburn@gmail.com>
2017-05-23 17:37:29 +1000

    Filter layersdisplayed in sidebar to only those in map-scope
    Closes SM-68

--- frontend/src/cljs/imas_seamap/map/subs.cljs
+++ frontend/src/cljs/imas_seamap/map/subs.cljs
@@ -1,10 +1,12 @@
 (ns imas-seamap.map.subs
-  (:require [reagent.core :as reagent]
+  (:require [imas-seamap.utils :refer [bbox-intersects?]]
+            [reagent.core :as reagent]
             [re-frame.core :as re-frame]))
 
 (defn map-props [db _] (:map db))
 
 (defn map-layers [db _]
-  (let [{:keys [layers active-layers]} (get-in db [:map])]
-    {:groups        (group-by :category layers)
+  (let [{:keys [layers active-layers bounds]} (get-in db [:map])
+        visible-layers                        (filter #(bbox-intersects? bounds (:bounding_box %)) layers)]
+    {:groups        (group-by :category visible-layers)
      :active-layers active-layers}))
