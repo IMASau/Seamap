40519293b8c5f74d59096f669f1aaa0037b563ff
Mark Hepburn <Mark.Hepburn@gmail.com>
2017-06-08 15:07:38 +1000

    Show "loading" message while we retrieve image metadata

--- frontend/src/cljs/imas_seamap/imgview/views.cljs
+++ frontend/src/cljs/imas_seamap/imgview/views.cljs
@@ -1,6 +1,7 @@
 (ns imas-seamap.imgview.views
   (:require [re-frame.core :as re-frame]
             [reagent.core :as reagent]
+            [imas-seamap.blueprint :as b]
             [debux.cs.core :refer [dbg]]))
 
 (def gallery (reagent/adapt-react-class js/Gallery))
@@ -18,8 +19,14 @@
                        :thumbnail-width  thumbnail-width
                        :thumbnail-height thumbnail-height})
                     images)]
-    [gallery {:images                   images
-              :is-open                  true
-              :enable-image-selection   false
-              :show-lightbox-thumbnails true}]))
+    (if (zero? (count images))
+      [:div.loading {:style {:width  "100%"
+                             :height "100vh"}}
+       [b/non-ideal-state {:visual (reagent/as-element
+                                    [b/spinner {:intent "success"}])
+                           :title  "Retrieving images..."}]]
+      [gallery {:images                   images
+                :is-open                  true
+                :enable-image-selection   false
+                :show-lightbox-thumbnails true}])))
 
