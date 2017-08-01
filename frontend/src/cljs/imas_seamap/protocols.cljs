15ea3c555aa31db6371760f947211719008c153e
Mark Hepburn <Mark.Hepburn@gmail.com>
2017-08-01 17:57:23 +1000

    Bugfix: firefox returns a different type from getBoundingClientRect

--- frontend/src/cljs/imas_seamap/protocols.cljs
+++ frontend/src/cljs/imas_seamap/protocols.cljs
@@ -14,7 +14,14 @@
   IEncodeClojure
   (-js->clj [o options] (jobj->dict o options)))
 
-;;; ClientRect is returned by the HTMLDivElement.getBoundingClientRect() method
-(extend-type js/ClientRect
-  IEncodeClojure
-  (-js->clj [o options] (jobj->dict o options)))
+;;; Types returned by the HTMLDivElement.getBoundingClientRect() method;
+;;; Chrome, Edge:
+(when (exists? js/ClientRect)
+  (extend-type js/ClientRect
+    IEncodeClojure
+    (-js->clj [o options] (jobj->dict o options))))
+;;; Firefox:
+(when (exists? js/DOMRect)
+  (extend-type js/DOMRect
+    IEncodeClojure
+    (-js->clj [o options] (jobj->dict o options))))
