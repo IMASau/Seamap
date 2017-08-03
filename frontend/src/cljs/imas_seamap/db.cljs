e912387048bb6ba60c14a083571a0baaa1441837
Mark Hepburn <Mark.Hepburn@gmail.com>
2017-08-03 12:34:36 +1000

    Whitespace (align initial db config)

--- frontend/src/cljs/imas_seamap/db.cljs
+++ frontend/src/cljs/imas_seamap/db.cljs
@@ -3,28 +3,28 @@
 (goog-define api-url-base "http://localhost:8000/api/")
 
 (def default-db
-  {:map {:center [-23.116667 132.133333]
-         :zoom 4
-         :zoom-cutover 5
-         :bounds {}
-         :layers []
-         :priorities []
-         :priority-cutoff 2 ; priorities <= this value will be displayed in auto mode
-         :groups []
-         :active-layers []
-         :logic {:type    :map.layer-logic/automatic
-                 :trigger :map.logic.trigger/automatic}
-         :controls {:transect false}}
-   :filters {:layers ""
-             :other-layers ""}
-   :transect {:query nil
-              :show? false
-              :habitat nil
-              :bathymetry nil}
+  {:map             {:center          [-23.116667 132.133333]
+                     :zoom            4
+                     :zoom-cutover    5
+                     :bounds          {}
+                     :layers          []
+                     :priorities      []
+                     :priority-cutoff 2 ; priorities <= this value will be displayed in auto mode
+                     :groups          []
+                     :active-layers   []
+                     :logic           {:type    :map.layer-logic/automatic
+                                       :trigger :map.logic.trigger/automatic}
+                     :controls        {:transect false}}
+   :filters         {:layers       ""
+                     :other-layers ""}
+   :transect        {:query      nil
+                     :show?      false
+                     :habitat    nil
+                     :bathymetry nil}
    :habitat-colours {}
-   :habitat-titles {}
-   :display {:help-overlay false}
-   :config {:layer-url (str api-url-base "layers/")
-            :group-url (str api-url-base "groups/")
-            :priority-url (str api-url-base "priorities/")
-            :descriptor-url (str api-url-base "descriptors/")}})
+   :habitat-titles  {}
+   :display         {:help-overlay false}
+   :config          {:layer-url      (str api-url-base "layers/")
+                     :group-url      (str api-url-base "groups/")
+                     :priority-url   (str api-url-base "priorities/")
+                     :descriptor-url (str api-url-base "descriptors/")}})
