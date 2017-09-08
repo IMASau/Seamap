8e2e329d1bc606b3c2a421bdd74cdf60d248900f
Mark Hepburn <Mark.Hepburn@gmail.com>
2017-09-08 16:56:42 +1000

    Add utils to parse and serialise key state. Ref SM-115
    Idea is we'll hook this into the URL hash, and load it at startup as
    well as update on map change

--- frontend/src/cljs/imas_seamap/utils.cljs
+++ frontend/src/cljs/imas_seamap/utils.cljs
@@ -1,5 +1,32 @@
-(ns imas-seamap.utils)
+(ns imas-seamap.utils
+  (:require [clojure.set :refer [rename-keys]]
+            [clojure.string :as string]
+            [clojure.walk :refer [keywordize-keys]]))
 
 ;;; http://blog.jayfields.com/2011/01/clojure-select-keys-select-values-and.html
 (defn select-values [map ks]
   (reduce #(conj %1 (map %2)) [] ks))
+
+(defn -equalise [[k v]] (str (name k) "=" v))
+
+(defn encode-state
+  "Returns a string suitable for storing in the URL's hash"
+  [{map-state :map :as db}]
+  (let [pruned (-> (select-keys map-state [:center :zoom :active-layers])
+                   (rename-keys {:active-layers :active})
+                   (update :center #(string/join ";" %))
+                   (update :active #(string/join ";" (map :layer_name %))))]
+    (->> pruned
+         (map -equalise)
+         (string/join "|"))))
+
+(defn parse-state [hash-str]
+  (let [parsed (->> (string/split hash-str "|")
+                    (map #(string/split % "="))
+                    (into {})
+                    keywordize-keys)]
+    (-> parsed
+        (update :center #(map js/parseFloat (string/split % ";")))
+        (update :active string/split ";")
+        (update :zoom js/parseInt)
+        (rename-keys {:active :active-layers}))))
