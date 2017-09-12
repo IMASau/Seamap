75ea1f70fc663b52535706fff7234aab7ebf8c2b
Mark Hepburn <Mark.Hepburn@gmail.com>
2017-09-12 17:58:28 +1000

    Coeffect to inject the parsed hash state. Ref SM-115
    Not committing the actual integraiton of this yet; I think it's best
    used just where needed, rather than globally so it constantly operates

--- frontend/src/cljs/imas_seamap/fx.cljs
+++ frontend/src/cljs/imas_seamap/fx.cljs
@@ -1,7 +1,7 @@
 (ns imas-seamap.fx
   (:require [clojure.string :as string]
             [oops.core :refer [gget gset!]]
-            [imas-seamap.utils :refer [encode-state]]
+            [imas-seamap.utils :refer [parse-state]]
             [re-frame.core :as re-frame]))
 
 (defn set-location-anchor [anchor]
@@ -9,3 +9,11 @@
 
 
 (re-frame/reg-fx :put-hash set-location-anchor)
+
+(defn cofx-hash-state [cofx _]
+  (let [hash-val (gget :location.hash)]
+    (merge cofx
+           (when-not (string/blank? hash-val)
+             {:hash-state (parse-state (subs hash-val 1))}))))
+
+(re-frame/reg-cofx :hash-state cofx-hash-state)
