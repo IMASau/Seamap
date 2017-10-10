9f1edc59ac6b139f8bf831375d019067bb93941e
Mark Hepburn <Mark.Hepburn@gmail.com>
2017-10-10 16:32:23 +1100

    Add trivial comment, while I'm momentarily having to think about it

--- frontend/src/cljs/imas_seamap/events.cljs
+++ frontend/src/cljs/imas_seamap/events.cljs
@@ -175,6 +175,7 @@
          "+proj=lcc +lat_1=-18 +lat_2=-36 +lat_0=0 +lon_0=134 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"))
 
 (defn- wgs48->epsg3112 [pt]
+  ;; pt is a vector of [lon lat]
   (js->clj
    (ocall *epsg-3112* :forward (clj->js pt))))
 
