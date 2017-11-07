768a7cb7109b870956f3e7a2eaf3218324e3ef66
Mark Hepburn <Mark.Hepburn@gmail.com>
2017-11-07 15:43:35 +1100

    Replace prj file in region-stats shpfile download. Closes SM-224
    Can't really see what the difference was, but this one worked where
    the original one didn't seem to (at least, wasn't automatically used
    in qgis)

--- backend/habitat/viewsets.py
+++ backend/habitat/viewsets.py
@@ -65,30 +65,7 @@ where region = %s
 """
 
 PRJ_3112 = """
-PROJCS["GDA94 / Geoscience Australia Lambert",
-    GEOGCS["GDA94",
-        DATUM["Geocentric_Datum_of_Australia_1994",
-            SPHEROID["GRS 1980",6378137,298.257222101,
-                AUTHORITY["EPSG","7019"]],
-            TOWGS84[0,0,0,0,0,0,0],
-            AUTHORITY["EPSG","6283"]],
-        PRIMEM["Greenwich",0,
-            AUTHORITY["EPSG","8901"]],
-        UNIT["degree",0.0174532925199433,
-            AUTHORITY["EPSG","9122"]],
-        AUTHORITY["EPSG","4283"]],
-    PROJECTION["Lambert_Conformal_Conic_2SP"],
-    PARAMETER["standard_parallel_1",-18],
-    PARAMETER["standard_parallel_2",-36],
-    PARAMETER["latitude_of_origin",0],
-    PARAMETER["central_meridian",134],
-    PARAMETER["false_easting",0],
-    PARAMETER["false_northing",0],
-    UNIT["metre",1,
-        AUTHORITY["EPSG","9001"]],
-    AXIS["Easting",EAST],
-    AXIS["Northing",NORTH],
-    AUTHORITY["EPSG","3112"]]
+PROJCS["GDA94_Geoscience_Australia_Lambert",GEOGCS["GCS_GDA_1994",DATUM["D_GDA_1994",SPHEROID["GRS_1980",6378137,298.257222101]],PRIMEM["Greenwich",0],UNIT["Degree",0.017453292519943295]],PROJECTION["Lambert_Conformal_Conic"],PARAMETER["standard_parallel_1",-18],PARAMETER["standard_parallel_2",-36],PARAMETER["latitude_of_origin",0],PARAMETER["central_meridian",134],PARAMETER["false_easting",0],PARAMETER["false_northing",0],UNIT["Meter",1]]
 """
 
 def D(number):
