5e04fb2389b6670efce1526342e5af12ff07f07d
Mark Hepburn <Mark.Hepburn@gmail.com>
2017-11-09 17:51:47 +1100

    Add leaflet-related externs from cljsjs

--- frontend/src/cljs/deps.cljs
+++ frontend/src/cljs/deps.cljs
@@ -1,3 +1,4 @@
-{:externs ["ext/react.ext.js" "ext/react-dom.ext.js" "ext/blueprint.ext.js"]
+{:externs ["ext/react.ext.js" "ext/react-dom.ext.js" "ext/blueprint.ext.js"
+           "ext/leaflet.ext.js" "ext/react-leaflet.ext.js" "ext/leaflet-draw.ext.js"]
  :foreign-libs [{:file "lib/bundled.js"
                  :provides ["cljsjs.react" "cljsjs.react.dom" "cljsjs.react.dom.server" "cljsjs.blueprint"]}]}
