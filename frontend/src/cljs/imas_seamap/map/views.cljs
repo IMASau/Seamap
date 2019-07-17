1e5f7401d6776bce5b1ab8605630038aba3940e8
Mark Hepburn <Mark.Hepburn@gmail.com>
2019-07-18 09:54:26 +1000

    ISA-17: copy-text has to be invoked directly inside the on-click
    That is, we can't define it in a handler because Firefox +
    Edge (strangely, Chrome is fine) won't do the clipboard manipulation
    outside of a short-lived (browser) event handler.

--- frontend/src/cljs/imas_seamap/map/views.cljs
+++ frontend/src/cljs/imas_seamap/map/views.cljs
@@ -5,7 +5,7 @@
   (:require [reagent.core :as r]
             [re-frame.core :as re-frame]
             [imas-seamap.blueprint :as b]
-            [imas-seamap.utils :refer [select-values] :refer-macros [handler-dispatch]]
+            [imas-seamap.utils :refer [copy-text select-values] :refer-macros [handler-dispatch]]
             [imas-seamap.map.utils :refer [sort-layers bounds->geojson download-type->str]]
             [oops.core :refer [ocall oget gget]]
             [debux.cs.core :refer-macros [dbg]]))
@@ -121,7 +121,12 @@
 
 (defn share-control [props]
   [custom-control {:position "topleft" :class-name "leaflet-bar"}
-   [:a {:on-click (handler-dispatch [:copy-share-url])}
+   ;; The copy-text has to be here rather than in a handler, because
+   ;; Firefox won't do execCommand('copy') outside of a "short-lived
+   ;; event handler"
+   [:a {:on-click #((copy-text js/location.href)
+                    (re-frame/dispatch  [:info/show-message "URL copied to clipboard!" {:intent   b/*intent-success*
+                                                                                        :iconName "clipboard"}]))}
     [b/tooltip {:content "Copy Shareable URL to clipboard" :position b/*RIGHT*}
      [b/icon {:icon-name "clipboard"}]]]])
 
