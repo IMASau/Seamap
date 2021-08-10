74efa48eb8b05c39208d8d257fc7db4f3a7b8a36
Mark Hepburn <Mark.Hepburn@gmail.com>
2021-08-10 10:54:09 +1000

    WIP: port build to shadow-cljs
    Currently having an issue with a babel issue relating to tether;
    duplicate definitions of the ClassCallCheck (_classCallCheck function)

--- frontend/src/cljs/deps.cljs
+++
@@ -1,7 +0,0 @@
-;;; Seamap: view and interact with Australian coastal habitat data
-;;; Copyright (c) 2017, Institute of Marine & Antarctic Studies.  Written by Condense Pty Ltd.
-;;; Released under the Affero General Public Licence (AGPL) v3.  See LICENSE file for details.
-{:externs ["ext/react.ext.js" "ext/react-dom.ext.js" "ext/blueprint.ext.js"
-           "ext/leaflet.ext.js" "ext/react-leaflet.ext.js" "ext/leaflet-draw.ext.js"]
- :foreign-libs [{:file "lib/bundled.js"
-                 :provides ["cljsjs.react" "cljsjs.react.dom" "cljsjs.react.dom.server" "cljsjs.blueprint"]}]}
