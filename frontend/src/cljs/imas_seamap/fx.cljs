4d61896245329b9eaea420b0912a80373b1f76c4
Mark Hepburn <Mark.Hepburn@gmail.com>
2021-08-18 18:17:38 +1000

    WIP: cofx: replace gset with direct use of js/ namespace

--- frontend/src/cljs/imas_seamap/fx.cljs
+++ frontend/src/cljs/imas_seamap/fx.cljs
@@ -3,18 +3,17 @@
 ;;; Released under the Affero General Public Licence (AGPL) v3.  See LICENSE file for details.
 (ns imas-seamap.fx
   (:require [clojure.string :as string]
-            [oops.core :refer [gget gset!]]
             [imas-seamap.utils :refer [parse-state]]
             [re-frame.core :as re-frame]))
 
 (defn set-location-anchor [anchor]
-  (gset! :location.hash anchor))
+  (set! js/location -hash anchor))
 
 (re-frame/reg-fx :put-hash set-location-anchor)
 
 
 (defn cofx-hash-state [cofx _]
-  (let [hash-val (gget :location.hash)]
+  (let [hash-val (. js/location -hash)]
     (merge cofx
            (when-not (string/blank? hash-val)
              {:hash-state (parse-state (subs hash-val 1))}))))
