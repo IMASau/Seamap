4c1e6195d0df745851df4737ab41cd6ec055fafb
Joshua Groombridge <joshua@condense.com.au>
2022-04-21 17:40:49 +1000

    (ISA-30) SelectionList interop

---
+++ frontend/src/cljs/imas_seamap/interop/ui_controls.cljs
@@ -0,0 +1,10 @@
+;;; Seamap: view and interact with Australian coastal habitat data
+;;; Copyright (c) 2021, Institute of Marine & Antarctic Studies.  Written by Condense Pty Ltd.
+;;; Released under the Affero General Public Licence (AGPL) v3.  See LICENSE file for details.
+(ns imas-seamap.interop.ui-controls
+  (:require [reagent.core :as reagent]
+            ["/SelectionList/SelectionList" :as SelectionList]))
+
+(assert SelectionList/SelectionList)
+
+(def SelectionList (reagent/adapt-react-class SelectionList/SelectionList))
