f9054a80da023e4fdf46513566ed4d5a199e8aa6
Joshua Groombridge <joshua@condense.com.au>
2022-05-04 13:27:18 +1000

    (ISA-103) Loading splash text changed

--- frontend/src/cljs/imas_seamap/events.cljs
+++ frontend/src/cljs/imas_seamap/events.cljs
@@ -66,7 +66,7 @@
 (defn loading-screen [db [_ msg]]
   (assoc db
          :loading true
-         :loading-message (or msg "Loading Seamap Layers...")))
+         :loading-message (or msg "Loading Tasmania Marine Atlas Layers...")))
 
 (defn application-loaded [db _]
   (assoc db :loading false))
