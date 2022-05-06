a1b7d7cd8ec42068cd7842a7b78cb85893e27ace
Joshua Groombridge <joshua@condense.com.au>
2022-05-06 15:07:26 +1000

    (ISA-101) Moved feature-info-none up

--- frontend/src/cljs/imas_seamap/map/utils.cljs
+++ frontend/src/cljs/imas_seamap/map/utils.cljs
@@ -212,6 +212,14 @@
                        :layers      (or detail_layer layer_name)})
         str)))
 
+(defn feature-info-none
+  []
+  {:info (str
+          "<div>"
+          "<h4>No info available</h4>"
+          "Layer summary not configured"
+          "</div>")})
+
 (defn feature-info-html
   [response]
   (let [parsed (.parseFromString (js/DOMParser.) response "text/html")
@@ -234,14 +242,6 @@
               "<table>" property-rows "</table>"
               "</div>")}
       {:status :feature-info/empty})))
-
-(defn feature-info-none
-  []
-  {:info (str
-          "<div>"
-          "<h4>No info available</h4>"
-          "Layer summary not configured"
-          "</div>")})
 
 (def info-format
   {1 "text/html"
