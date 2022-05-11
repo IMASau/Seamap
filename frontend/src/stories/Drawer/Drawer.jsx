46f81625abad06521f62a7fe80eb66136934b2ee
Joshua Groombridge <joshua@condense.com.au>
2022-05-11 13:33:59 +1000

    (ISA-120) React Drawer component complete

--- frontend/src/stories/Drawer/Drawer.jsx
+++ frontend/src/stories/Drawer/Drawer.jsx
@@ -2,11 +2,42 @@ import React from 'react';
 import PropTypes from "prop-types";
 import * as BPCore from '@blueprintjs/core';
 
-export function Drawer({isOpen}) {
+export function Drawer({isOpen, title, onClose, children, position, size}) {
+	const positionValue = (
+		function(position) {
+			switch(position) {
+				case "TOP":
+				case "Top":
+				case "top":
+				case "t":
+					return BPCore.Position.TOP;
+				case "BOTTOM":
+				case "Bottom":
+				case "bottom":
+				case "b":
+					return BPCore.Position.BOTTOM;
+				case "LEFT":
+				case "Left":
+				case "left":
+				case "l":
+					return BPCore.Position.LEFT;
+				default:
+					return BPCore.Position.RIGHT;
+			}
+		}
+	)(position);
+
 	return (
 		<BPCore.Drawer
 			isOpen={isOpen}
+			title={title}
+			position={positionValue}
+			size={size}
+			onClose={() => onClose ? onClose() : null}
+			isCloseButtonShown={true}
+			canOutsideClickClose={false}
 		>
+			{children}
 		</BPCore.Drawer>
 	);
 }
