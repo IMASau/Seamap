3a5c6c991ec6e40d963c5e4396311b28377bd94a
Joshua Groombridge <joshua@condense.com.au>
2022-06-03 13:10:14 +1000

    (ISA-126) adapting leaflet scale factor, and fixing code issue (reading height of element that has no height)

--- frontend/src/thirdparty/leaflet.scalefactor.js
+++ frontend/src/thirdparty/leaflet.scalefactor.js
@@ -1,82 +1,101 @@
 // Code taken from leaflet.ScaleFactor by Marc Chasse (https://github.com/MarcChasse/leaflet.ScaleFactor)
 
-(function (factory, window) {
-
-    // define an AMD module that relies on 'leaflet'
+(function (factory) {
+    var L;
     if (typeof define === 'function' && define.amd) {
+        // AMD
         define(['leaflet'], factory);
-
-    // define a Common JS module that relies on 'leaflet'
-    } else if (typeof exports === 'object') {
-        module.exports = factory(require('leaflet'));
-    }
-
-    // attach your plugin to the global 'L' variable
-    if (typeof window !== 'undefined' && window.L) {
-        window.L.Control.ScaleFactor = factory(L);
-        L.control.scalefactor = function (options) {
-            return new window.L.Control.ScaleFactor(options);
-        };
+    } else if (typeof module !== 'undefined') {
+        // Node/CommonJS
+        L = require('leaflet');
+        module.exports = factory(L);
+    } else {
+        // Browser globals
+        if (typeof window.L === 'undefined') {
+            throw new Error('Leaflet must be loaded first');
+        }
+        factory(window.L);
     }
 }(function (L) {
-    var ScaleFactor = L.Control.extend({
-        options: {
-            position: 'bottomleft',
-            updateWhenIdle: true
-        },
+    'use strict';
 
-        onAdd: function (map) {
-            var className = 'leaflet-control-scalefactor',
-                container = L.DomUtil.create('div', className),
-                options = this.options;
+    L.Control.ScaleFactor = (function () {
+        var ScaleFactor = L.Control.extend({
+            options: {
+                position: 'bottomleft',
+                updateWhenIdle: true
+            },
 
-            this._mScale = L.DomUtil.create('div', className + '-line', container);
+            onAdd: function (map) {
+                var className = 'leaflet-control-scalefactor',
+                    container = L.DomUtil.create('div', className),
+                    options = this.options;
 
-            map.on(options.updateWhenIdle ? 'moveend' : 'move', this._update, this);
-            map.whenReady(this._update, this);
+                this._mScale = L.DomUtil.create('div', className + '-line', container);
 
-            return container;
-        },
+                map.on(options.updateWhenIdle ? 'moveend' : 'move', this._update, this);
+                map.whenReady(this._update, this);
 
-        onRemove: function (map) {
-            map.off(this.options.updateWhenIdle ? 'moveend' : 'move', this._update, this);
-        },
+                return container;
+            },
 
-        _pxTOmm: (function () {
-            var heightRef = document.createElement('div');
-            heightRef.style = 'height:1mm;display:none';
-            heightRef.id = 'heightRef';
-            document.body.appendChild(heightRef);
+            onRemove: function (map) {
+                map.off(this.options.updateWhenIdle ? 'moveend' : 'move', this._update, this);
+            },
 
-            heightRef = document.getElementById('heightRef');
-            var pxPermm = $('#heightRef').height();
+            _pxTOmm: (function () {
+                var heightRef = document.createElement('div');
+                heightRef.style = 'height:1mm;';
+                heightRef.id = 'heightRef';
+                document.body.appendChild(heightRef);
 
-            heightRef.parentNode.removeChild(heightRef);
+                heightRef = document.getElementById('heightRef');
+                console.log(heightRef)
+                console.log(heightRef.parentNode)
+                var pxPermm = heightRef.offsetHeight;
+                console.log("pxPermm")
+                console.log(pxPermm)
 
-            return function pxTOmm(px) {
-                return px / pxPermm;
+                //heightRef.parentNode.removeChild(heightRef);
+
+                return function pxTOmm(px) {
+                    return px / pxPermm;
+                }
+            })(),
+
+            _update: function () {
+                var map = this._map;
+
+                var CenterOfMap = map.getSize().y / 2;
+
+                var RealWorlMetersPer100Pixels = map.distance(
+                    map.containerPointToLatLng([0, CenterOfMap]),
+                    map.containerPointToLatLng([100, CenterOfMap])
+                );
+
+                var ScreenMetersPer100Pixels = this._pxTOmm(100) / 1000;
+
+                var scaleFactor = RealWorlMetersPer100Pixels / ScreenMetersPer100Pixels;
+
+                //.replace formats the scale with commas 50000 -> 50,000
+                console.log(RealWorlMetersPer100Pixels)
+                console.log(ScreenMetersPer100Pixels)
+                this._mScale.innerHTML = '1:' + Math.round(scaleFactor).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
             }
-        })(),
 
-        _update: function () {
-            var map = this._map;
+        });
 
-            var CenterOfMap = map.getSize().y / 2;
+        return ScaleFactor;
+    })();
 
-            var RealWorlMetersPer100Pixels = map.distance(
-                map.containerPointToLatLng([0, CenterOfMap]),
-                map.containerPointToLatLng([100, CenterOfMap])
-            );
-
-            var ScreenMetersPer100Pixels = this._pxTOmm(100) / 1000;
-
-            var scaleFactor = RealWorlMetersPer100Pixels / ScreenMetersPer100Pixels;
-
-            //.replace formats the scale with commas 50000 -> 50,000
-            this._mScale.innerHTML = '1:' + Math.round(scaleFactor).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    L.Map.addInitHook(function () {
+        if (this.options.scaleFactor) {
+            this.scaleFactor = new L.Control.ScaleFactor();
+            this.addControl(this.scaleFactor);
         }
-
     });
 
-    return ScaleFactor;
-}, window));
+    L.control.scaleFactor = function (options) {
+        return new L.Control.ScaleFactor(options);
+    };
+}));
