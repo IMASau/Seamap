5b455904b18b77d00b3eb75a8d4544bc7a88e4ae
Joshua Groombridge <joshua@condense.com.au>
2022-06-10 12:08:23 +1000

    (ISA-158) select value field merged into simple select field

--- frontend/src/ui/SelectField/SelectValueField.stories.jsx
+++ frontend/src/ui/SelectField/SelectValueField.stories.jsx
@@ -1,73 +1,73 @@
 import React from 'react';
 
-import {SelectValueField} from './SelectField';
+import {SimpleSelectField} from './SelectField';
 import './SelectField.css';
 import '@blueprintjs/core/lib/css/blueprint.css';
 import NOTES from './NOTES.mdx';
 
 export default {
-    title: 'SelectField/SelectValueField',
-    component: SelectValueField,
-    argTypes: {
-        // Most are inferred from propTypes
-        onChange: {action: 'onChange'},
-        onBlur: {action: 'onBlur'},
-    }
+	title: 'SelectField/SimpleSelectField',
+	component: SimpleSelectField,
+	argTypes: {
+		// Most are inferred from propTypes
+		onChange: {action: 'onChange'},
+		onBlur: {action: 'onBlur'},
+	}
 };
 
-const FieldTemplate = (args) => <SelectValueField {...args} />;
+const FieldTemplate = (args) => <SimpleSelectField {...args} />;
 
 const options = [
-    {value: 'chocolate', label: 'Chocolate'},
-    {value: 'strawberry', label: 'Strawberry'},
-    {value: 'vanilla', label: 'Vanilla'}
+	{value: 'chocolate', label: 'Chocolate'},
+	{value: 'strawberry', label: 'Strawberry'},
+	{value: 'vanilla', label: 'Vanilla'}
 ]
 
 export const SimpleField = FieldTemplate.bind({});
 SimpleField.args = {
-    value: null,
-    options: options,
-    placeholder: "",
-    disabled: false,
-    hasError: false,
-    getValue: (option => option.value),
-    getLabel: (option => option.label)
+	value: null,
+	options: options,
+	placeholder: "",
+	disabled: false,
+	hasError: false,
+	getValue: (option => option.value),
+	getLabel: (option => option.label)
 };
 
 export const FieldDisabledState = FieldTemplate.bind({});
 FieldDisabledState.args = {
-    value: options[0].value,
-    options: options,
-    disabled: true,
-    getValue: (option => option.value),
-    getLabel: (option => option.label)
+	value: options[0].value,
+	options: options,
+	disabled: true,
+	getValue: (option => option.value),
+	getLabel: (option => option.label)
 };
 
 export const FieldWithError = FieldTemplate.bind({});
 FieldWithError.args = {
-    value: options[0].value,
-    options: options,
-    hasError: true,
-    getValue: (option => option.value),
-    getLabel: (option => option.label)
+	value: options[0].value,
+	options: options,
+	hasError: true,
+	getValue: (option => option.value),
+	getLabel: (option => option.label)
 };
 
 export const FieldWithInvalidValue = FieldTemplate.bind({});
 FieldWithInvalidValue.args = {
-    value: 'marzipan',
-    options: options,
-    hasError: true,
-    getValue: (option => option.value),
-    getLabel: (option => option.label)
+	value: 'marzipan',
+	options: options,
+	hasError: true,
+	getValue: (option => option.value),
+	getLabel: (option => option.label)
 };
 
 
 export const EmptyFieldWithPlaceholder = FieldTemplate.bind({});
 EmptyFieldWithPlaceholder.args = {
-    placeholder: "This is the placeholder",
-    options: options,
-    getValue: (option => option.value),
-    getLabel: (option => option.label)
+	placeholder: "This is the placeholder",
+	options: options,
+	getValue: (option => option.value),
+	getLabel: (option => option.label)
 };
 
 export const DesignDecisions = NOTES;
