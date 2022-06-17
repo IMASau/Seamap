a0ca3ebe53f402732d1c218ad10ade79b6dc50af
Joshua Groombridge <joshua@condense.com.au>
2022-06-17 11:33:02 +1000

    (ISA-161) simplified BoundaryAreas view

--- database/Views/SeamapAus_BoundaryAreas_View.sql
+++ database/Views/SeamapAus_BoundaryAreas_View.sql
@@ -1,18 +1,9 @@
 CREATE VIEW [dbo].[SeamapAus_BoundaryAreas_View] AS
 SELECT
-  [network],
-  [park],
-  [zone],
-  [zone_iucn],
-  SUM([area]) AS [area]
-FROM (
-  SELECT
-    [network],
-    [park],
-    [zone],
-    [zone_iucn],
-    MAX([boundary_area]) AS [area]
-  FROM [IMASSeamap].[dbo].[SeamapAus_HabitatStatistics_View]
-  GROUP BY [polygon_id], [network], [park], [zone], [zone_iucn]
-) AS [BoundaryAreas]
-GROUP BY [network], [park], [zone], [zone_iucn];
+  [NETNAME] AS [network],
+  [RESNAME] AS [park],
+  [ZONENAME] AS [zone],
+  [ZONEIUCN] AS [zone_iucn],
+  SUM([geom].STArea()) AS [area]
+FROM [IMASSeamap].[dbo].[SeamapAus_AMP2018_ZONES_UNIQUE_codepreserve]
+GROUP BY [NETNAME], [RESNAME], [ZONENAME], [ZONEIUCN];
