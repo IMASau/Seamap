df5d228b902255370b66141ee87a32ec5e416b84
Joshua Groombridge <joshua@condense.com.au>
2022-07-01 15:51:01 +1000

    (ISA-185) BOUNDARY_IMCRA_HABITAT

---
+++ database/Tables/BOUNDARY_IMCRA_HABITAT.sql
@@ -0,0 +1,27 @@
+-- Pre-calculated geometries of habitats per IMCRA boundary. 
+
+CREATE TABLE [dbo].[BOUNDARY_IMCRA_HABITAT] (
+  [Provincial_Bioregion] NVARCHAR(255) NOT NULL,
+  [Mesoscale_Bioregion]  NVARCHAR(255) NULL,
+  [geom]                 GEOMETRY      NOT NULL
+);
+
+INSERT INTO [dbo].[BOUNDARY_IMCRA_HABITAT] ([Provincial_Bioregion], [Mesoscale_Bioregion], [habitat], [geom])
+SELECT
+  [boundary].[Provincial_Bioregion],
+  [boundary].[Mesoscale_Bioregion],
+  [habitat].[CATEGORY] AS [habitat],
+  [habitat].[geom]
+FROM [dbo].[VW_BOUNDARY_IMCRA] AS [boundary]
+CROSS APPLY [dbo].habitat_intersections([boundary].[geom]) AS [habitat];
+
+-- Use:
+-- DECLARE @provincial_bioregion NVARCHAR(255) = 'Cocos (Keeling) Island Province';
+-- DECLARE @mesoscale_bioregion  NVARCHAR(255) = NULL;
+
+-- SELECT [habitat], geometry::UnionAggregate([geom])
+-- FROM [dbo].[BOUNDARY_AMP_HABITAT]
+-- WHERE
+--   ([Provincial_Bioregion] = @provincial_bioregion OR @provincial_bioregion IS NULL) AND
+--   ([Mesoscale_Bioregion] = @mesoscale_bioregion OR @mesoscale_bioregion IS NULL)
+-- GROUP BY [habitat];
