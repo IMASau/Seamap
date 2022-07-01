6fd7a705975c702f878b9f2542cd6bfc1384411a
Joshua Groombridge <joshua@condense.com.au>
2022-07-01 15:23:32 +1000

    (ISA-185) VW_BOUNDARY_IMCRA

---
+++ database/Views/VW_BOUNDARY_IMCRA.sql
@@ -0,0 +1,14 @@
+-- Contains the geometries for the smallest possible unit of IMCRA boundaries.
+-- Geometries can be selected by any combination of network, park, or zone.
+
+CREATE VIEW [dbo].[VW_BOUNDARY_IMCRA] AS
+  SELECT
+    [pb_name] AS [Provincial_Bioregion],
+    (
+      CASE
+        WHEN meso_name = 'nil' THEN NULL
+        ELSE meso_name
+      END
+    ) AS [Mesoscale_Bioregion],
+    [geom]
+  FROM [SeamapAus_BOUNDARIES_IMCRA_FULL]
