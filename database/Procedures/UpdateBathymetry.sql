81486979938ccb63d563a7fc3d6ed9c15e0def39
Joshua Groombridge <joshua@condense.com.au>
2022-07-01 16:52:34 +1000

    (ISA-185) IMCRA boundaries for UpdateBathymetry

--- database/Procedures/UpdateBathymetry.sql
+++ database/Procedures/UpdateBathymetry.sql
@@ -1,6 +1,6 @@
 -- When a bathymetry category has been updated, use this stored procedure to update
--- the hardcoded values in the BathymetryGeoms, UniqueBathymetryGeoms, and
--- BOUNDARY_AMP_BATHYMETRY tables.
+-- the hardcoded values in the BathymetryGeoms, UniqueBathymetryGeoms,
+-- BOUNDARY_AMP_BATHYMETRY, and BOUNDARY_IMCRA_BATHYMETRY tables.
 
 CREATE PROCEDURE UpdateBathymetry
   @category VARCHAR(10)
@@ -42,6 +42,19 @@ BEGIN
     [bathymetry].[RANK] AS [bathymetry_rank],
     [bathymetry].[geom]
   FROM [dbo].[VW_BOUNDARY_AMP] AS [boundary]
+  CROSS APPLY [dbo].unique_bathymetry_intersections([boundary].[geom]) AS [bathymetry]
+  WHERE [bathymetry].[CATEGORY] = @category;
+
+  -- Update BOUNDARY_IMCRA_BATHYMETRY
+  DELETE FROM [dbo].[BOUNDARY_IMCRA_BATHYMETRY] WHERE [bathymetry_category] = @category;
+  INSERT INTO [dbo].[BOUNDARY_IMCRA_BATHYMETRY] ([Provincial_Bioregion], [Mesoscale_Bioregion], [bathymetry_category], [bathymetry_rank], [geom])
+  SELECT
+    [boundary].[Provincial_Bioregion],
+    [boundary].[Mesoscale_Bioregion],
+    [bathymetry].[CATEGORY] AS [bathymetry_category],
+    [bathymetry].[RANK] AS [bathymetry_rank],
+    [bathymetry].[geom]
+  FROM [dbo].[VW_BOUNDARY_IMCRA] AS [boundary]
   CROSS APPLY [dbo].unique_bathymetry_intersections([boundary].[geom]) AS [bathymetry]
   WHERE [bathymetry].[CATEGORY] = @category;
 END;
