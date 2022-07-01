992855fa03af478d740daf28cf443bf722cc5e53
Joshua Groombridge <joshua@condense.com.au>
2022-07-01 15:25:55 +1000

    (ISA-185) adjust indents

--- database/Views/VW_BOUNDARY_AMP.sql
+++ database/Views/VW_BOUNDARY_AMP.sql
@@ -2,10 +2,10 @@
 -- Geometries can be selected by any combination of network, park, or zone.
 
 CREATE VIEW [dbo].[VW_BOUNDARY_AMP] AS
-  SELECT
-    [NETNAME] AS [Network],
-    [RESNAME] AS [Park],
-    [ZONENAME] AS [Zone_Category],
-    [ZONEIUCN] AS [IUCN_Zone],
-    [geom]
-  FROM [dbo].[SeamapAus_BOUNDARIES_AMP2022];
+SELECT
+  [NETNAME] AS [Network],
+  [RESNAME] AS [Park],
+  [ZONENAME] AS [Zone_Category],
+  [ZONEIUCN] AS [IUCN_Zone],
+  [geom]
+FROM [dbo].[SeamapAus_BOUNDARIES_AMP2022];
