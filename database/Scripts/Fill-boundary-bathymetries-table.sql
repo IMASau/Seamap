468888c5743be13a8fa26112e5f3a1c035efa901
Joshua Groombridge <joshua@condense.com.au>
2022-07-01 12:15:52 +1000

    (ISA-185) BoundaryBathymetries using VW_BOUNDARY_AMP

--- database/Scripts/Fill-boundary-bathymetries-table.sql
+++ database/Scripts/Fill-boundary-bathymetries-table.sql
@@ -12,14 +12,14 @@ CREATE TABLE [dbo].[BoundaryBathymetries] (
 
 INSERT INTO [dbo].[BoundaryBathymetries] ([NETNAME], [RESNAME], [ZONENAME], [ZONEIUCN], [bathymetry_category], [bathymetry_rank], [geom])
 SELECT
-  [boundary].[NETNAME],
-  [boundary].[RESNAME],
-  [boundary].[ZONENAME],
-  [boundary].[ZONEIUCN],
+  [boundary].[Network],
+  [boundary].[Park],
+  [boundary].[Zone_Category],
+  [boundary].[IUCN_Zone],
   [bathymetry].[CATEGORY] AS [bathymetry_category],
   [bathymetry].[RANK] AS [bathymetry_rank],
   [bathymetry].[geom]
-FROM [dbo].[BoundaryGeoms_View] AS [boundary]
+FROM [dbo].[VW_BOUNDARY_AMP] AS [boundary]
 CROSS APPLY [dbo].unique_bathymetry_intersections([boundary].[geom]) AS [bathymetry];
 
 -- Use:
