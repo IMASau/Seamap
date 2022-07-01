a1e888e0c4e989b61c6373066c6287da62ea9dae
Joshua Groombridge <joshua@condense.com.au>
2022-07-01 13:04:27 +1000

    (ISA-185) Use comment added to BOUNDARY_AMP_HABITAT

--- database/Tables/BOUNDARY_AMP_HABITAT.sql
+++ database/Tables/BOUNDARY_AMP_HABITAT.sql
@@ -19,3 +19,18 @@ SELECT
   [habitat].[geom]
 FROM [dbo].[VW_BOUNDARY_AMP] AS [boundary]
 CROSS APPLY [dbo].habitat_intersections([boundary].[geom]) AS [habitat];
+
+-- Use:
+-- DECLARE @network   NVARCHAR(254) = 'Coral Sea';
+-- DECLARE @park      NVARCHAR(254) = NULL;
+-- DECLARE @zone      NVARCHAR(254) = NULL;
+-- DECLARE @zone_iucn NVARCHAR(5)   = NULL;
+
+-- SELECT [habitat], geometry::UnionAggregate([geom])
+-- FROM [dbo].[BOUNDARY_AMP_HABITAT]
+-- WHERE
+--   ([Network] = @network OR @network IS NULL) AND
+--   ([Park] = @park OR @park IS NULL) AND
+--   ([Zone_Category] = @zone OR @zone IS NULL) AND
+--   ([IUCN_Zone] = @zone_iucn OR @zone_iucn IS NULL)
+-- GROUP BY [habitat];
