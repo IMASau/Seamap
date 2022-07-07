79a8c36cfaf191abaf18bf61442a60f981dbd6e6
Joshua Groombridge <joshua@condense.com.au>
2022-07-07 10:29:16 +1000

    (ISA-161) amp boundaries habitat observations intersections

---
+++ database/Tables/BOUNDARY_AMP_HABITAT_OBS_SQUIDLE.sql
@@ -0,0 +1,33 @@
+-- Pre-calculated SQUIDLE habitat observations per AMP boundary.
+
+CREATE TABLE [dbo].[BOUNDARY_AMP_HABITAT_OBS_SQUIDLE] (
+  [Network]       NVARCHAR(254) NOT NULL,
+  [Park]          NVARCHAR(254) NOT NULL,
+  [Zone_Category] NVARCHAR(254) NOT NULL,
+  [IUCN_Zone]     NVARCHAR(5)   NOT NULL,
+  [observation]   NVARCHAR(480) NOT NULL
+);
+
+INSERT INTO [dbo].[BOUNDARY_AMP_HABITAT_OBS_SQUIDLE] ([Network], [Park], [Zone_Category], [IUCN_Zone], [observation])
+  [boundary].[Network],
+  [boundary].[Park],
+  [boundary].[Zone_Category],
+  [boundary].[IUCN_Zone],
+  [observation].[DEPLOYMENT_ID] AS [observation]
+FROM [dbo].[VW_BOUNDARY_AMP] AS [boundary]
+CROSS APPLY [dbo].HABITAT_OBS_SQUIDLE_intersections([boundary].[geom]) AS [observation];
+
+-- Use:
+-- DECLARE @network   NVARCHAR(254) = 'Coral Sea';
+-- DECLARE @park      NVARCHAR(254) = NULL;
+-- DECLARE @zone      NVARCHAR(254) = NULL;
+-- DECLARE @zone_iucn NVARCHAR(5)   = NULL;
+
+-- SELECT [observation]
+-- FROM [dbo].[BOUNDARY_AMP_HABITAT_OBS_SQUIDLE]
+-- WHERE
+--   ([Network] = @network OR @network IS NULL) AND
+--   ([Park] = @park OR @park IS NULL) AND
+--   ([Zone_Category] = @zone OR @zone IS NULL) AND
+--   ([IUCN_Zone] = @zone_iucn OR @zone_iucn IS NULL)
+-- GROUP BY [observation];
