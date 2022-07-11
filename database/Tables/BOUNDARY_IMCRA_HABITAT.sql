faaef050163d6067970e29d887050c792aa550b3
Joshua Groombridge <joshua@condense.com.au>
2022-07-11 14:59:10 +1000

    (ISA-161) precomputed area column added to precomputed tables

--- database/Tables/BOUNDARY_IMCRA_HABITAT.sql
+++ database/Tables/BOUNDARY_IMCRA_HABITAT.sql
@@ -4,15 +4,17 @@ CREATE TABLE [dbo].[BOUNDARY_IMCRA_HABITAT] (
   [Provincial_Bioregion] NVARCHAR(255) NOT NULL,
   [Mesoscale_Bioregion]  NVARCHAR(255) NULL,
   [habitat]              NVARCHAR(30)  NOT NULL,
-  [geom]                 GEOMETRY      NOT NULL
+  [geom]                 GEOMETRY      NOT NULL,
+  [area]                 FLOAT         NOT NULL
 );
 
-INSERT INTO [dbo].[BOUNDARY_IMCRA_HABITAT] ([Provincial_Bioregion], [Mesoscale_Bioregion], [habitat], [geom])
+INSERT INTO [dbo].[BOUNDARY_IMCRA_HABITAT] ([Provincial_Bioregion], [Mesoscale_Bioregion], [habitat], [geom], [area])
 SELECT
   [boundary].[Provincial_Bioregion],
   [boundary].[Mesoscale_Bioregion],
   [habitat].[CATEGORY] AS [habitat],
-  [habitat].[geom]
+  [habitat].[geom],
+  [habitat].[geom].STArea() AS [area]
 FROM [dbo].[VW_BOUNDARY_IMCRA] AS [boundary]
 CROSS APPLY [dbo].habitat_intersections([boundary].[geom]) AS [habitat];
 
@@ -20,7 +22,7 @@ CROSS APPLY [dbo].habitat_intersections([boundary].[geom]) AS [habitat];
 -- DECLARE @provincial_bioregion NVARCHAR(255) = 'Cocos (Keeling) Island Province';
 -- DECLARE @mesoscale_bioregion  NVARCHAR(255) = NULL;
 
--- SELECT [habitat], geometry::UnionAggregate([geom])
+-- SELECT [habitat], [area]
 -- FROM [dbo].[BOUNDARY_IMCRA_HABITAT]
 -- WHERE
 --   ([Provincial_Bioregion] = @provincial_bioregion OR @provincial_bioregion IS NULL) AND
