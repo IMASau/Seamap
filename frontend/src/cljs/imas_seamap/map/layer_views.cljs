d59975291eddfc511aeb96319d56bacf3052f5b0
Joshua Groombridge <joshua@condense.com.au>
2022-08-31 15:00:16 +1000

    layer-header text and status icons

--- frontend/src/cljs/imas_seamap/map/layer_views.cljs
+++ frontend/src/cljs/imas_seamap/map/layer_views.cljs
@@ -1,9 +1,29 @@
 (ns imas-seamap.map.layer-views
-  (:require [imas-seamap.blueprint :as b]
+  (:require [re-frame.core :as re-frame]
+            [imas-seamap.blueprint :as b]
             #_[debux.cs.core :refer [dbg] :include-macros true]))
 
-(defn- layer-header [{{:keys [name]} :layer}]
-  [:div.layer-header name])
+(defn- layer-status-icons [{:keys [loading? errors?]}]
+  (when (or loading? errors?)
+   [:div.layer-status-icons
+    (when loading? [b/spinner {:class "bp3-text-muted bp3-small"}])
+    (when errors? [b/icon {:icon "warning-sign" :class "bp3-text-muted bp3-small"}])]))
+
+(defn- layer-header-text [{{:keys [name] :as layer} :layer {:keys [expanded? active?]} :other-props}]
+  [b/tooltip
+   {:content (if expanded? "Hide details" "Show details")
+    :disabled (not active?)
+    :class "layer-header-text"}
+   [b/clipped-text
+    {:ellipsize true
+     :on-click  #(re-frame/dispatch [:map.layer.legend/toggle layer])}
+    name]])
+
+(defn- layer-header [{:keys [_layer] {:keys [active? visible?] :as other-props} :other-props :as layer-props}]
+  [:div.layer-header
+   (when (and active? visible?)
+     [layer-status-icons other-props])
+   [layer-header-text layer-props]])
 
 (defn- layer-content [{:keys [_layer _other-props] :as layer-props}]
   [:div.layer-content
