2b01533e3fbdfea66858ad9a2412d3e536d37b05
Joshua Groombridge <joshua@condense.com.au>
2022-08-31 14:58:45 +1000

    layer-header start

--- frontend/src/cljs/imas_seamap/map/layer_views.cljs
+++ frontend/src/cljs/imas_seamap/map/layer_views.cljs
@@ -2,7 +2,15 @@
   (:require [imas-seamap.blueprint :as b]
             #_[debux.cs.core :refer [dbg] :include-macros true]))
 
-(defn layer-card [{:keys [_layer] {:keys [active?]} :other-props}]
+(defn- layer-header [{{:keys [name]} :layer}]
+  [:div.layer-header name])
+
+(defn- layer-content [{:keys [_layer _other-props] :as layer-props}]
+  [:div.layer-content
+   [layer-header layer-props]])
+
+(defn layer-card [{:keys [_layer] {:keys [active?]} :other-props :as layer-props}]
   [b/card
    {:elevation 1
-    :class (str "layer-card" (when active? " active-layer"))}])
+    :class (str "layer-card" (when active? " active-layer"))}
+   [layer-content layer-props]])
