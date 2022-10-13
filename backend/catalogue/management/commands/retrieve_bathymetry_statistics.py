b71131b5ae9ecffe02d0bdd638433cc18844a998
Joshua Groombridge <joshua@condense.com.au>
2022-10-13 11:10:58 +1100

    (ISA-317) Retrieve bathymetry statistics management command

--- backend/catalogue/management/commands/retrieve_bathymetry_statistics.py
+++ backend/catalogue/management/commands/retrieve_bathymetry_statistics.py
@@ -22,58 +22,60 @@ FROM VW_BOUNDARY_AMP;
 
 SQL_GET_AMP_BOUNDARY_AREA = "SELECT dbo.AMP_BOUNDARY_geom(%s, %s, NULL, NULL).STArea() / 1000000"
 
-SQL_GET_AMP_HABITAT_STATS = """
+SQL_GET_AMP_BATHYMETRY_STATS = """
 DECLARE @netname       NVARCHAR(254) = %s;
 DECLARE @resname       NVARCHAR(254) = %s;
 DECLARE @boundary_area FLOAT         = %s;
 
 SELECT
-  boundary.habitat,
+  boundary.resolution,
+  boundary.rank,
   boundary.area,
   boundary.mapped_percentage,
   boundary.total_percentage,
   descriptor.colour AS color
 FROM (
   SELECT
-    habitat,
+    bathymetry_resolution as resolution,
+    bathymetry_rank as rank,
     SUM(area) / 1000000 AS area,
     100 * SUM(area) / (
       SELECT SUM(area)
-      FROM BOUNDARY_AMP_HABITAT
+      FROM BOUNDARY_AMP_BATHYMETRY
       WHERE
         (Network = @netname OR @netname IS NULL) AND
         (Park = @resname OR @resname IS NULL)
     ) AS mapped_percentage,
     100 * (SUM(area) / 1000000) / @boundary_area AS total_percentage
-  FROM BOUNDARY_AMP_HABITAT
+  FROM BOUNDARY_AMP_BATHYMETRY
   WHERE
     (Network = @netname OR @netname IS NULL) AND
     (Park = @resname OR @resname IS NULL)
-  GROUP BY habitat
+  GROUP BY bathymetry_resolution, bathymetry_rank
 ) AS boundary
 JOIN catalogue_habitatdescriptor AS descriptor
-ON boundary.habitat = descriptor.name;
+ON boundary.resolution = descriptor.name;
 """
 
-def generate_habitat_statistics(cursor, network=None, park=None):
-    logging.info(f'Retrieving habitat statistics for {network} - {park}' if park else f'Retrieving habitat statistics for {network}')
+def generate_bathymetry_statistics(cursor, network=None, park=None):
+    logging.info(f'Retrieving bathymetry statistics for {network} - {park}' if park else f'Retrieving bathymetry statistics for {network}')
 
-    filepath = f'habitat_statistics/{network}/{park}.json' if park else f'habitat_statistics/{network}.json'
+    filepath = f'bathymetry_statistics/{network}/{park}.json' if park else f'bathymetry_statistics/{network}.json'
 
     cursor.execute(SQL_GET_AMP_BOUNDARY_AREA, [network, park])
 
     boundary_area = float(cursor.fetchone()[0])
-    cursor.execute(SQL_GET_AMP_HABITAT_STATS, [network, park, boundary_area])
+    cursor.execute(SQL_GET_AMP_BATHYMETRY_STATS, [network, park, boundary_area])
 
     columns = [col[0] for col in cursor.description]
     namedrow = namedtuple('Result', columns)
     results = [namedrow(*row) for row in cursor.fetchall()]
-    habitat_statistics = [row._asdict() for row in results]
+    bathymetry_statistics = [row._asdict() for row in results]
 
     # saving
-    logging.info(f'Saving habitat statistics for {network} - {park}' if park else f'Saving habitat statistics for {network}')
+    logging.info(f'Saving bathymetry statistics for {network} - {park}' if park else f'Saving bathymetry statistics for {network}')
     default_storage.delete(filepath)
-    default_storage.save(filepath, ContentFile(json.dumps(habitat_statistics)))
+    default_storage.save(filepath, ContentFile(json.dumps(bathymetry_statistics)))
 
 
 class Command(BaseCommand):
@@ -95,10 +97,10 @@ class Command(BaseCommand):
             results = [namedrow(*row) for row in cursor.fetchall()]
             parks = [row._asdict() for row in results]
 
-            # retrieve habitat statistics
+            # retrieve bathymetry statistics
             for network in networks:
-                generate_habitat_statistics(cursor, network['network'])
+                generate_bathymetry_statistics(cursor, network['network'])
             
             for park in parks:
-                generate_habitat_statistics(cursor, park['network'], park['park'])
+                generate_bathymetry_statistics(cursor, park['network'], park['park'])
             
