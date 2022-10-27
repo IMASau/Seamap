55c6f9e8c76f268f431731c0cad7ccb2777cecc2
Mark Hepburn <Mark.Hepburn@gmail.com>
2022-10-28 10:58:50 +1100

    ISA-178: include broken WFS url in error email
    (Untested yet, it's going to be as quick to push and deploy and test
    on the server!)

--- etl/email_manager.py
+++ etl/email_manager.py
@@ -9,7 +9,7 @@ def send_error_email(source_name, error):
     try:
         s = smtplib.SMTP(config['email']['smtp_server'])
         msg = EmailMessage()
-        msg.set_content(f"There was an error when handling {source_name}:\n {error}")
+        msg.set_content(f"There was an error when handling {source_name}:\n {error}\n\nEndpoint: {config[source_name]['url']}")
         msg['Subject'] = 'Error in Seamap ETL'
         msg['To'] = config['email']['recipients']
         msg['From'] = config['email']['sender']
