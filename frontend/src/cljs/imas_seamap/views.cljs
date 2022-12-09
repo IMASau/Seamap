deb405cb93e99b03cbd84a4529b5ead9c394b57b
Joshua Groombridge <joshua@condense.com.au>
2022-12-09 12:54:08 +1100

    (ISA-334) keyboard shortcuts dialogue styled

--- frontend/src/cljs/imas_seamap/views.cljs
+++ frontend/src/cljs/imas_seamap/views.cljs
@@ -639,6 +639,7 @@
   (let [keydown-wrapper
         (fn [m keydown-v]
           (assoc m :global    true
+                 :group "Keyboard Shortcuts"
                  :onKeyDown #(re-frame/dispatch keydown-v)))]
     ;; See note on `use-hotkeys' for rationale invoking `clj->js' here:
     (clj->js
