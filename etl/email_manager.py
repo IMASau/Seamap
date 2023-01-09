8ea8849747e057e346a32d12818a6f360b21586a
Khanh Nguyen <khanh@condense.com.au>
2023-01-09 11:43:59 +1100

    IWA-955: capture database connection issue

--- etl/email_manager.py
+++ etl/email_manager.py
@@ -17,3 +17,17 @@ def send_error_email(source_name, error):
         s.quit()
     except Exception:
         logging.exception("Error sending email")
+
+
+def send_generic_error_email(error):
+    try:
+        s = smtplib.SMTP(config['email']['smtp_server'])
+        msg = EmailMessage()
+        msg.set_content(f"There was an error in Seamap ETL:\n {error}")
+        msg['Subject'] = 'Error in Seamap ETL'
+        msg['To'] = config['email']['recipients']
+        msg['From'] = config['email']['sender']
+        s.send_message(msg)
+        s.quit()
+    except Exception:
+        logging.exception("Error sending email")
--- etl/main.py
+++ etl/main.py
@@ -1,23 +1,36 @@
 import logging
 
+import pyodbc
+
+from settings import config
 from extractor import extract
 from transformer import transform
-from email_manager import send_error_email
+from email_manager import send_error_email, send_generic_error_email
 
 
 def main():
-    sources = ['squidle', 'mars', 'global_archive']
-
-    for source in sources:
-        try:
-            logging.info(f"Extracting {source}")
-            extract(source)
-            logging.info(f"Transforming {source}")
-            transform(source)
-        except Exception as e:
-            logging.error(f"An error happened when processing {source}! Sending email notification")
-            # send email and continue to next source
-            send_error_email(source, str(e))
+    cnxn = None
+    try:
+        # open a connection to the database
+        cnxn = pyodbc.connect(config['database']['connection_string'])
+        cnxn.autocommit = False
+        sources = ['squidle', 'mars', 'global_archive']
+        for source in sources:
+            try:
+                logging.info(f"Extracting {source}")
+                extract(cnxn, source)
+                logging.info(f"Transforming {source}")
+                transform(cnxn, source)
+            except Exception as e:
+                logging.error(f"An error happened when processing {source}! Sending email notification")
+                # send email and continue to next source
+                send_error_email(source, str(e))
+    except Exception as e:
+        logging.error("An error occurred")
+        send_generic_error_email(str(e))
+    finally:
+        if cnxn is not None:
+            cnxn.close()
 
 
 if __name__ == '__main__':
