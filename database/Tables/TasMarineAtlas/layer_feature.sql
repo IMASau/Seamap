dcad7437d67e45d0a754699a19f53e64007ccc5e
Joshua Groombridge <joshua@condense.com.au>
2023-01-18 16:59:13 +1100

    (ISA-396) added layer feature index table

---
+++ database/Tables/TasMarineAtlas/layer_feature.sql
@@ -0,0 +1,5 @@
+CREATE TABLE [dbo].[layer_feature] (
+  [layer_id]   INT           NOT NULL FOREIGN KEY REFERENCES [dbo].[catalogue_layer]([id]),
+  [feature_id] NVARCHAR(MAX) NOT NULL,
+  [geom]       GEOMETRY      NOT NULL,
+);
