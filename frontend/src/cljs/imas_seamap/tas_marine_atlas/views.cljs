93368f6e70353626f8a0ad0e7fbf8eaed22a5ff1
Joshua Groombridge <joshua@condense.com.au>
2023-08-28 16:22:03 +1000

    (ISA-509) "Layer info / Download data" -> "Layer info" for TMA only

--- frontend/src/cljs/imas_seamap/tas_marine_atlas/views.cljs
+++ frontend/src/cljs/imas_seamap/tas_marine_atlas/views.cljs
@@ -147,7 +147,7 @@
         :class "catalogue"
         :title (reagent/as-element
                 [b/tooltip {:content "All available map layers"} "Catalogue"])
-        :panel (reagent/as-element [left-drawer-catalogue])}]
+        :panel (reagent/as-element [left-drawer-catalogue true])}]
 
       [b/tab
        {:id    "active-layers"
@@ -156,7 +156,7 @@
                  [:<> "Active Layers"
                   (when (seq active-layers)
                     [:div.notification-bubble (count active-layers)])]])
-        :panel (reagent/as-element [left-drawer-active-layers])}]
+        :panel (reagent/as-element [left-drawer-active-layers true])}]
 
       [b/tab
        {:id    "featured-maps"
@@ -201,7 +201,7 @@
      (case status
 
        :data-in-region/loaded
-       [layer-catalogue :region layers layer-props]
+       [layer-catalogue :region layers layer-props true]
        #_[:<>
         (for [{:keys [id] :as layer} layers]
           ^{:key (str id)}
