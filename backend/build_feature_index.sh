723cff1c4e07e830917fef0e140f8c42a75c0ab3
Joshua Groombridge <joshua@condense.com.au>
2023-08-30 11:08:13 +1000

    (ISA-441) build_feature_index optional argument for layer to add/update

--- backend/build_feature_index.sh
+++ backend/build_feature_index.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-OUTPUT=$($1 $2 build_feature_index_start | tail -1)
+if [[ $3 ]]; then
+    OUTPUT=$($1 $2 build_feature_index_start --layer_id $3 | tail -1)
+else
+    OUTPUT=$($1 $2 build_feature_index_start | tail -1)
+fi
 IFS=' ' read -ra ADDR <<< "$OUTPUT"
 for i in "${ADDR[@]}"; do
     $1 $2 build_feature_index_layer --layer_id $i
--- backend/catalogue/management/commands/build_feature_index_start.py
+++ backend/catalogue/management/commands/build_feature_index_start.py
@@ -7,7 +7,14 @@ from catalogue.models import Layer
 # Clears the temp layer feature table
 SQL_TRUNCATE_LAYER_FEATURE_TEMP = "TRUNCATE TABLE layer_feature_temp;"
 
+
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--layer_id',
+            help="Optional parameter: outputs this layer for use in build_feature_index_layer, instead of all layers."
+        )
+
     def handle(self, *args, **options):
         try:
             with connections['transects'].cursor() as cursor:
@@ -15,4 +22,8 @@ class Command(BaseCommand):
         except Exception as e:
             logging.error('Error at %s', 'division', exc_info=e)
         else:
-            print(' '.join(str(layer.id) for layer in Layer.objects.all()))
+            print(
+                options['layer_id']
+                if options['layer_id']
+                else ' '.join(str(layer.id) for layer in Layer.objects.all())
+            )
