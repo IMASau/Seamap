6d194aa43e114c7ae857a8be11069127d7c7b1bc
Mark Hepburn <Mark.Hepburn@gmail.com>
2023-09-14 14:41:54 +1000

    ISA-473: experimental worker to parse ESRI data into geojson
    The issue seems to be that the response taking a long time (eg 20s) to
    return is fine, but the parsing / conversion to geojson can also take
    quite an appreciable amount of time, and since that happens inside the
    xhr callback it all blocks up the task queue and locks up the browser.
    We spawn the parsing and conversion into a separate thread (a Worker),
    and the remaining callback is simple the callback invoked on the
    worker's response message, freeing up resources.
    
    Note that there's no thread management (such as a pool) other than
    cleaning up each one after it has finished, and the processing is
    still pretty heavy on resources.
    
    To achieve all this: we vendor the esri-leaflet library and make that
    small change to use a worker, then write a worker that is just the
    parsing code mentioned (mostly from @terraformer/arcgis)
    
    The bigger complications are integrating into seamap's build process,
    using shadow-cljs.  Shadow has support for building workers, but there
    are issues with hashed module names for eg.
    
    We may yet introduce parceljs or similar to simplify the worker
    compilation, as it seems more efficient than shadow.

---
+++ frontend/src/worker/esri-leaflet-worker/Parser.js
@@ -0,0 +1,87 @@
+import { arcgisToGeoJSON } from '@terraformer/arcgis';
+
+
+// Util.js
+// We can't import {EsriUtil} from 'esri-leaflet' here because that
+// tries to access the window object which doesn't exist in a worker
+// context:
+var knownFieldNames = /^(OBJECTID|FID|OID|ID)$/i;
+
+// Attempts to find the ID Field from response
+function _findIdAttributeFromResponse (response) {
+  var result;
+
+  if (response.objectIdFieldName) {
+    // Find Id Field directly
+    result = response.objectIdFieldName;
+  } else if (response.fields) {
+    // Find ID Field based on field type
+    for (var j = 0; j <= response.fields.length - 1; j++) {
+      if (response.fields[j].type === 'esriFieldTypeOID') {
+        result = response.fields[j].name;
+        break;
+      }
+    }
+    if (!result) {
+      // If no field was marked as being the esriFieldTypeOID try well known field names
+      for (j = 0; j <= response.fields.length - 1; j++) {
+        if (response.fields[j].name.match(knownFieldNames)) {
+          result = response.fields[j].name;
+          break;
+        }
+      }
+    }
+  }
+  return result;
+}
+
+// This is the 'last' resort, find the Id field from the specified feature
+function _findIdAttributeFromFeature (feature) {
+  for (var key in feature.attributes) {
+    if (key.match(knownFieldNames)) {
+      return key;
+    }
+  }
+}
+
+export function responseToFeatureCollection (response, idAttribute) {
+  var objectIdField;
+  var features = response.features || response.results;
+  var count = features && features.length;
+
+  if (idAttribute) {
+    objectIdField = idAttribute;
+  } else {
+    objectIdField = _findIdAttributeFromResponse(response);
+  }
+
+  var featureCollection = {
+    type: 'FeatureCollection',
+    features: []
+  };
+
+  if (count) {
+    for (var i = features.length - 1; i >= 0; i--) {
+      var feature = arcgisToGeoJSON(features[i], objectIdField || _findIdAttributeFromFeature(features[i]));
+      featureCollection.features.push(feature);
+    }
+  }
+
+  return featureCollection;
+}
+
+
+// ////////////////////////////////////////////////////////////////
+// // web-worker interface
+
+// console.log('**** worker loaded');
+// self.addEventListener('message', function(e) {
+//   var data = e.data;
+
+//   console.log('**** worker');
+//   self.postMessage(responseToFeatureCollection(data));
+//   // I think this causes timing issues where the thread dies before
+//   // the event has been received and deletes the payload...something
+//   // like that?:
+//   // self.close();
+// }, false);
