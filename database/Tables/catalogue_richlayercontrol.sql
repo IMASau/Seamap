1738c2cbbeb898a1b2b7df003649823c852499ab
Joshua Groombridge <joshua@condense.com.au>
2024-06-05 15:07:48 +1000

    ISA-575: add richlayercontrol table

---
+++ database/Tables/catalogue_richlayercontrol.sql
@@ -0,0 +1,10 @@
+CREATE TABLE [dbo].[catalogue_richlayercontrol] (
+  [id]              INT           NOT NULL PRIMARY KEY IDENTITY(1,1),
+  [richlayer_id]    INT           NOT NULL FOREIGN KEY REFERENCES [dbo].[catalogue_richlayer]([id]),
+  [cql_property]    NVARCHAR(MAX) NOT NULL,
+  [label]           NVARCHAR(MAX) NOT NULL,
+  [data_type]       NVARCHAR(MAX) NOT NULL,
+  [controller_type] NVARCHAR(MAX) NOT NULL,
+  [icon]            NVARCHAR(MAX) NOT NULL,
+  [tooltip]         NVARCHAR(MAX) NOT NULL
+);
