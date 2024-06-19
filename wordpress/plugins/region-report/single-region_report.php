2be8fdfa429eb5d6c30c01688cc37dd00fdd566f
Joshua Groombridge <joshua@condense.com.au>
2024-06-19 13:31:58 +1000

    ISA-567: Fixed region report imagery annotations class name

--- wordpress/plugins/region-report/single-region_report.php
+++ wordpress/plugins/region-report/single-region_report.php
@@ -183,7 +183,7 @@
                         <div>Highlights</div>
                     </div>
                 </div>
-                <div class="images" id="region-report-imagery-annotations-<?php the_ID(); ?>">
+                <div class="imagery-annotations" id="region-report-imagery-annotations-<?php the_ID(); ?>">
                     Loading imagery deployment data...
                 </div>
             </div>
