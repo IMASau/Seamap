086505066991dbf1ca65ffb9abba428524c715e0
Joshua Groombridge <joshua@condense.com.au>
2024-07-19 15:40:32 +1000

    ISA-572: Remove static directory assumptions from carbonabatementsidebar view

--- backend/carbonabatementsidebar/views.py
+++ backend/carbonabatementsidebar/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.views.decorators.cache import never_cache
 from django.views.decorators.clickjacking import xframe_options_exempt
 import json
+import os
 
 @never_cache
 @xframe_options_exempt
@@ -14,11 +15,13 @@ def carbon_abatement_sidebar(request):
         context.update({'DEBUG': True})
         return render(request, 'index.html', context)
     else:
-        with open(f'{settings.STATIC_ROOT}/carbonabatementsidebar/vite/manifest.json') as f:
+        with open(os.path.join(settings.STATIC_ROOT, 'carbonabatementsidebar/vite/manifest.json')) as f:
             manifest = json.load(f)
+            main_js = os.path.join('carbonabatementsidebar', manifest['src/main.tsx']['file'])
+            main_css = os.path.join('carbonabatementsidebar', manifest['src/main.tsx']['css'][0])
         context.update({
-            'main_js': f'carbonabatementsidebar/{manifest["src/main.tsx"]["file"]}',
-            'main_css': f'carbonabatementsidebar/{manifest["src/main.tsx"]["css"][0]}',
+            'main_js': main_js,
+            'main_css': main_css,
             'DEBUG': False
         })
         return render(request, 'index.html', context)
--- backend/webapp/settings.py
+++ backend/webapp/settings.py
@@ -128,7 +128,7 @@ CORS_ORIGIN_WHITELIST = []
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 SQL_ROOT = os.path.join(BASE_DIR, "../database")
 
