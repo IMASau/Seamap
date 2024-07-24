018579b7f9ea8c1433a62601cec05ece248c3b97
Joshua Groombridge <joshua@condense.com.au>
2024-07-24 14:31:22 +1000

    ISA-572: include blueprint in sidebar

--- backend/carbonabatementsidebar/frontend/src/main.tsx
+++ backend/carbonabatementsidebar/frontend/src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App/App.tsx'
 
+import '@blueprintjs/core/lib/css/blueprint.css';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <App />
