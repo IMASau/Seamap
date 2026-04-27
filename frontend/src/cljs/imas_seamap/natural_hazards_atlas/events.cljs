2527aed1e8de97c2bcfbb7153c0b4bf66439c031
Joshua Groombridge <joshua@condense.com.au>
2026-04-27 17:58:41 +1000

    NHAT-12: Adding natural-hazards-atlas core Starting point based on the futures-of-seafood core. Trying to keep things obvious on how this app differs from others for when we want to work on #294

---
+++ frontend/src/cljs/imas_seamap/natural_hazards_atlas/events.cljs
@@ -0,0 +1,223 @@
+;;; Seamap: view and interact with Australian coastal habitat data
+;;; Copyright (c) 2017, Institute of Marine & Antarctic Studies.  Written by Condense Pty Ltd.
+;;; Released under the Affero General Public Licence (AGPL) v3.  See LICENSE file for details.
+(ns imas-seamap.natural-hazards-atlas.events
+  (:require [ajax.core :as ajax]
+            [imas-seamap.db :as db]
+            [imas-seamap.utils :refer [merge-in ajax-loaded-info]]
+            #_[debux.cs.core :refer [dbg] :include-macros true]))
+
+(defn- boot-flow
+  "Like imas-seamap.events/boot-flow, but we don't dispatch the events that load
+   region reports and state of knowledge data since those aren't present in the
+   Natural Hazards Atlas."
+  []
+  {:first-dispatch [:ui/show-loading]
+   :rules
+   [{:when :seen? :events :ui/show-loading :dispatch [:construct-urls]}
+    {:when :seen? :events :construct-urls :dispatch [:initialise-layers]}
+    {:when :seen-all-of? :events [:map/update-base-layers
+                                  :map/update-base-layer-groups]
+     :dispatch [:map/update-grouped-base-layers]}
+    {:when :seen-all-of? :events [:map/update-layers
+                                  :map/update-keyed-layers
+                                  :map/update-rich-layers]
+     :dispatch-n [[:map/join-keyed-layers]
+                  [:map/join-rich-layers]]}
+    {:when :seen-all-of? :events [:map/update-grouped-base-layers
+                                  :map/update-layers
+                                  :map/update-organisations
+                                  :map/update-classifications
+                                  :map/update-descriptors
+                                  :map/update-categories
+                                  :map/update-keyed-layers
+                                  :map/join-keyed-layers
+                                  :map/join-rich-layers]
+     :dispatch-n [[:map/initialise-display]
+                  [:transect/maybe-query]]}
+    {:when :seen? :events :ui/hide-loading
+     :dispatch-n [[:welcome-layer/open]
+                  [:display.outage-message/open true]]
+     :halt? true}
+    {:when :seen-any-of? :events [:ajax/default-err-handler] :dispatch [:loading-failed] :halt? true}]})
+
+(defn- boot-flow-hash-state
+    "Like imas-seamap.events/boot-flow-hash-state, but we don't dispatch the events
+     that load region reports and state of knowledge data since those aren't present
+     in the Natural Hazards Atlas."
+  [hash-code]
+  {:first-dispatch [:ui/show-loading]
+   :rules
+   [{:when :seen? :events :ui/show-loading :dispatch [:construct-urls]}
+    {:when :seen? :events :construct-urls :dispatch [:load-hash-state hash-code]}
+    {:when :seen? :events :load-hash-state :dispatch [:initialise-layers]}
+    {:when :seen-all-of? :events [:map/update-base-layers
+                                  :map/update-base-layer-groups]
+     :dispatch [:map/update-grouped-base-layers]}
+    {:when :seen-all-of? :events [:map/update-layers
+                                  :map/update-keyed-layers
+                                  :map/update-rich-layers]
+     :dispatch-n [[:map/join-keyed-layers]
+                  [:map/join-rich-layers]]}
+    {:when :seen-all-of? :events [:map/update-grouped-base-layers
+                                  :map/update-layers
+                                  :map/update-organisations
+                                  :map/update-classifications
+                                  :map/update-descriptors
+                                  :map/update-categories
+                                  :map/update-keyed-layers
+                                  :map/join-keyed-layers
+                                  :map/join-rich-layers]
+     :dispatch-n [[:map/initialise-display]
+                  [:transect/maybe-query]]}
+    {:when :seen? :events :ui/hide-loading
+     :dispatch-n [[:welcome-layer/open]
+                  [:display.outage-message/open true]]
+     :halt? true}
+    {:when :seen-any-of? :events [:ajax/default-err-handler] :dispatch [:loading-failed] :halt? true}]})
+
+(defn- boot-flow-save-state
+  "Like imas-seamap.events/boot-flow-save-state, but we don't dispatch the events
+   that load region reports and state of knowledge data since those aren't present
+   in the Natural Hazards Atlas."
+  [shortcode]
+  {:first-dispatch [:ui/show-loading]
+   :rules
+   [{:when :seen? :events :ui/show-loading :dispatch [:construct-urls]}
+    {:when :seen? :events :construct-urls :dispatch [:get-save-state shortcode [:load-hash-state]]}
+    {:when :seen? :events :load-hash-state :dispatch [:initialise-layers]}
+    {:when :seen-all-of? :events [:map/update-base-layers
+                                  :map/update-base-layer-groups]
+     :dispatch [:map/update-grouped-base-layers]}
+    {:when :seen-all-of? :events [:map/update-layers
+                                  :map/update-keyed-layers
+                                  :map/update-rich-layers]
+     :dispatch-n [[:map/join-keyed-layers]
+                  [:map/join-rich-layers]]}
+    {:when :seen-all-of? :events [:map/update-grouped-base-layers
+                                  :map/update-layers
+                                  :map/update-organisations
+                                  :map/update-classifications
+                                  :map/update-descriptors
+                                  :map/update-categories
+                                  :map/update-keyed-layers
+                                  :map/join-keyed-layers
+                                  :map/join-rich-layers]
+     :dispatch-n [[:map/initialise-display]
+                  [:transect/maybe-query]]}
+    {:when :seen? :events :ui/hide-loading
+     :dispatch-n [[:welcome-layer/open]
+                  [:display.outage-message/open true]]
+     :halt? true}
+    {:when :seen-any-of? :events [:ajax/default-err-handler] :dispatch [:loading-failed] :halt? true}]})
+
+(defn boot
+  "Identical to imas-seamap.events/boot, just triggers the private versions of the
+   boot flow functions that don't load region reports and state of knowledge data."
+  [{:keys [save-code hash-code] {:keys [seamap-app-state]} :local-storage/get} [_ api-url-base media-url-base wordpress-url-base img-url-base]]
+  {:db         (assoc-in
+                db/default-db [:config :url-base]
+                {:api-url-base       api-url-base
+                 :media-url-base     media-url-base
+                 :wordpress-url-base wordpress-url-base
+                 :img-url-base       img-url-base})
+   :async-flow (cond ; Choose async boot flow based on what information we have for the DB:
+                 (seq save-code)    (boot-flow-save-state save-code)    ; A shortform save-code that can be used to query for a hash-code
+                 (seq hash-code)    (boot-flow-hash-state hash-code)    ; A hash-code that can be decoded into th DB's initial state
+                 (seq seamap-app-state) (boot-flow-hash-state seamap-app-state) ; Same as hash-code, except that we use the one stored in local storage
+                 :else              (boot-flow))})                      ; No information, so we start with an empty DB
+
+(defn re-boot
+  "Identical to imas-seamap.events/re-boot, just triggers the private versions of
+   the boot flow functions that don't load region reports and state of knowledge
+   data."
+ [{:keys [db]} _]
+  (let [db (merge-in db/default-db (ajax-loaded-info db))
+        startup-layers (get-in db [:map :keyed-layers :startup] [])
+        db (-> db
+               (assoc-in [:map :active-layers] startup-layers)
+               (assoc-in [:map :active-base-layer] (first (get-in db [:map :grouped-base-layers])))
+               (assoc :initialised true))
+        {:keys [zoom center]} (:map db)]
+    {:db         db
+     :dispatch   [:map/update-map-view (if (seq startup-layers) {:bounds (:bounding_box (first startup-layers))} {:zoom zoom :center center})]
+     :local-storage/remove
+     {:name :seamap-app-state}}))
+
+(defn initialise-layers
+  "Like imas-seamap.events/initialise-layers, but we don't dispatch requests for
+   region reports or state of knowledge data."
+  [{:keys [db]} _]
+  (let [{:keys [site-configuration-url
+                layer-url
+                base-layer-url
+                base-layer-group-url
+                organisation-url
+                classification-url
+                descriptor-url
+                category-url
+                keyed-layers-url
+                rich-layers-url
+                dynamic-pills-url
+                story-maps-url]} (get-in db [:config :urls])]
+    {:db         db
+     :http-xhrio [{:method          :get
+                   :uri             site-configuration-url
+                   :response-format (ajax/json-response-format {:keywords? true})
+                   :on-success      [:update-site-configuration]
+                   :on-failure      [:update-site-configuration/error-handler]}
+                  {:method          :get
+                   :uri             layer-url
+                   :response-format (ajax/json-response-format {:keywords? true})
+                   :on-success      [:map/update-layers]
+                   :on-failure      [:ajax/default-err-handler]}
+                  {:method          :get
+                   :uri             base-layer-url
+                   :response-format (ajax/json-response-format {:keywords? true})
+                   :on-success      [:map/update-base-layers]
+                   :on-failure      [:ajax/default-err-handler]}
+                  {:method          :get
+                   :uri             base-layer-group-url
+                   :response-format (ajax/json-response-format {:keywords? true})
+                   :on-success      [:map/update-base-layer-groups]
+                   :on-failure      [:ajax/default-err-handler]}
+                  {:method          :get
+                   :uri             descriptor-url
+                   :response-format (ajax/json-response-format {:keywords? true})
+                   :on-success      [:map/update-descriptors]
+                   :on-failure      [:ajax/default-err-handler]}
+                  {:method          :get
+                   :uri             classification-url
+                   :response-format (ajax/json-response-format {:keywords? true})
+                   :on-success      [:map/update-classifications]
+                   :on-failure      [:ajax/default-err-handler]}
+                  {:method          :get
+                   :uri             organisation-url
+                   :response-format (ajax/json-response-format {:keywords? true})
+                   :on-success      [:map/update-organisations]
+                   :on-failure      [:ajax/default-err-handler]}
+                  {:method          :get
+                   :uri             category-url
+                   :response-format (ajax/json-response-format {:keywords? true})
+                   :on-success      [:map/update-categories]
+                   :on-failure      [:ajax/default-err-handler]}
+                  {:method          :get
+                   :uri             keyed-layers-url
+                   :response-format (ajax/json-response-format {:keywords? true})
+                   :on-success      [:map/update-keyed-layers]
+                   :on-failure      [:ajax/default-err-handler]}
+                  {:method          :get
+                   :uri             rich-layers-url
+                   :response-format (ajax/json-response-format {:keywords? true})
+                   :on-success      [:map/update-rich-layers]
+                   :on-failure      [:ajax/default-err-handler]}
+                  {:method          :get
+                   :uri             dynamic-pills-url
+                   :response-format (ajax/json-response-format {:keywords? true})
+                   :on-success      [:update-dynamic-pills]
+                   :on-failure      [:ajax/default-err-handler]}
+                  {:method          :get
+                   :uri             story-maps-url
+                   :response-format (ajax/json-response-format {:keywords? true})
+                   :on-success      [:sm/update-featured-maps]
+                   :on-failure      [:sm/update-featured-maps []]}]}))
