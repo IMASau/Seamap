72fd8ae1f8feaaab5c590310343d8c1d361c736f
Joshua Groombridge <joshua@condense.com.au>
2026-05-18 15:50:22 +1000

    NHAT-16: Register nhat signals

--- backend/nhat/apps.py
+++ backend/nhat/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class NhatConfig(AppConfig):
     name = 'nhat'
+
+    def ready(self):
+        import nhat.signals # pylint: disable=import-outside-toplevel, unused-import
