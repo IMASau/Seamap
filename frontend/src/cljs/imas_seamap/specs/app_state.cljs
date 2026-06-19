9c8b933940b70795f0c40a3e679fc7a17e68c873
Joshua Groombridge <joshua@condense.com.au>
2026-06-19 17:06:04 +1000

    NATHAZ-62: Add current view to app state spec

--- frontend/src/cljs/imas_seamap/specs/app_state.cljs
+++ frontend/src/cljs/imas_seamap/specs/app_state.cljs
@@ -673,3 +673,84 @@
                    ::map
                    ::dynamic-pills
                    ::transect]))
+
+;;; current view
+(s/def :current-view.cmip-phase/id integer?)
+(s/def :current-view.cmip-phase/name string?)
+(s/def :current-view.cmip-phase/sort_key (s/nilable string?))
+(s/def :current-view/cmip-phase
+  (s/keys
+   :req-un
+   [:current-view.cmip-phase/id
+    :current-view.cmip-phase/name
+    :current-view.cmip-phase/sort_key]))
+(s/def :current-view/cmip-phases
+  (s/coll-of :current-view/cmip-phase :kind vector?))
+
+(s/def :current-view.model/id integer?)
+(s/def :current-view.model/name string?)
+(s/def :current-view.model/sort_key (s/nilable string?))
+(s/def :current-view/model
+  (s/keys
+   :req-un
+   [:current-view.model/id
+    :current-view.model/name
+    :current-view.model/sort_key]))
+(s/def :current-view/models
+  (s/coll-of :current-view/model :kind vector?))
+
+(s/def :current-view.scenario/id integer?)
+(s/def :current-view.scenario/name string?)
+(s/def :current-view.scenario/sort_key (s/nilable string?))
+(s/def :current-view/scenario
+  (s/keys
+   :req-un
+   [:current-view.scenario/id
+    :current-view.scenario/name
+    :current-view.scenario/sort_key]))
+(s/def :current-view/scenarios
+  (s/coll-of :current-view/scenario :kind vector?))
+
+(s/def :current-view.seasonal-data/id integer?)
+(s/def :current-view.seasonal-data/name string?)
+(s/def :current-view.seasonal-data/sort_key (s/nilable string?))
+(s/def :current-view/seasonal-data
+  (s/keys
+   :req-un
+   [:current-view.seasonal-data/id
+    :current-view.seasonal-data/name
+    :current-view.seasonal-data/sort_key]))
+(s/def :current-view/seasonal-datas
+  (s/coll-of :current-view/seasonal-data :kind vector?))
+
+(s/def :current-view/selected-cmip-phase-id (s/nilable :current-view.cmip-phase/id))
+(s/def :current-view/selected-model-id (s/nilable :current-view.model/id))
+(s/def :current-view/selected-scenario-id (s/nilable :current-view.scenario/id))
+(s/def :current-view/selected-seasonal-data-id (s/nilable :current-view.seasonal-data/id))
+
+(s/def ::current-view
+  (s/keys
+   :req-un
+   [:current-view/cmip-phases
+    :current-view/models
+    :current-view/scenarios
+    :current-view/seasonal-datas
+    :current-view/selected-cmip-phase-id
+    :current-view/selected-model-id
+    :current-view/selected-scenario-id
+    :current-view/selected-seasonal-data-id]))
+
+(s/def :nhat/app-state ; unused
+  (s/keys :req-un [::config
+                   ::site-configuration
+                   ::display
+                   ::story-maps
+                   ::filters
+                   ::region-stats
+                   ::habitat-colours
+                   ::habitat-titles
+                   ::layer-state
+                   ::map
+                   ::dynamic-pills
+                   ::transect
+                   ::current-view]))
