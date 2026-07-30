003e103bbcac20189782699d68d3dc805f3202d2
Mark Hepburn <Mark.Hepburn@gmail.com>
2026-07-30 17:24:37 +1000

    feat: replace re-frame-cookie-fx with re-implementation
    The main issue is the original is abandoned, and now depends on a
    deprecated singleton instance goog.net.cookies.  It's not that
    complicated, so let's just re-implement it (with the help of Claude),
    keeping the same interface, or at least the bits we use

--- frontend/src/cljs/imas_seamap/fx.cljs
+++ frontend/src/cljs/imas_seamap/fx.cljs
@@ -2,7 +2,8 @@
 ;;; Copyright (c) 2017, Institute of Marine & Antarctic Studies.  Written by Condense Pty Ltd.
 ;;; Released under the Affero General Public Licence (AGPL) v3.  See LICENSE file for details.
 (ns imas-seamap.fx
-  (:require [imas-seamap.blueprint :as b]
+  (:require [goog.net.Cookies]
+            [imas-seamap.blueprint :as b]
             [imas-seamap.utils :refer [uuid4?]]
             [re-frame.core :as re-frame]))
 
@@ -55,3 +56,55 @@
             (assoc acc name (js/window.localStorage.getItem (cljs.core/name name))))
           {} names)]
      (assoc cofx :local-storage/get values))))
+
+;;; Cookies (replaces com.smxemail/re-frame-cookie-fx which is
+;;; abandoned / relies on deprecated functionality in the closure
+;;; library):
+
+;; Helper to get a new Cookies instance
+(defn- get-cookies-instance []
+  (.getInstance goog.net.Cookies))
+
+;; Helper to convert a keyword to a string, or return the input if already a string
+(defn- kw->str [k]
+  (if (keyword? k) (name k) k))
+
+;; Define a default expiration (e.g., 1 year from now)
+(defn- default-expires-date []
+  (let [now (js/Date.)
+        one-year-ms (* 365 24 60 60 1000)] ; milliseconds in one year
+    (js/Date. (+ (.getTime now) one-year-ms))))
+
+;; --- COFX Handler: :cookies/get ---
+;; Usage: (re-frame/inject-cofx :cookies/get [:seen-welcome])
+;; This will add {:cookies/get {:seen-welcome "cookie-value"} to the cofx map.
+(re-frame/reg-cofx
+ :cookie/get
+ (fn [cofx names]
+   (let [cookies (get-cookies-instance)]
+     (assoc cofx
+            :cookie/get
+            (reduce #(into %1 {(keyword %2) (.get cookies (name %2))})
+                    {}
+                    names))))) ; Associates values under the original keywordised names
+
+;; --- COFX Handler: :cookies/set ---
+;; Usage: (re-frame/inject-cofx :cookies/set {:name :seen-welcome :value true :expires (js/Date. 0)})
+;; Options like :expires, :path, :domain, :secure, :sameSite are optional.
+;; :expires can be a js/Date object or a number (milliseconds from epoch).
+(re-frame/reg-fx
+  :cookie/set
+  (fn [{:keys [name value expires path domain secure sameSite] :as opts}]
+    (let [cookies (get-cookies-instance)
+          cookie-name-str (kw->str name)
+          cookie-value-str (str value) ; Cookie values are typically strings
+          effective-expires (or expires (default-expires-date))
+          effective-path (or path "/")]
+      (.set cookies
+            cookie-name-str
+            cookie-value-str
+            effective-expires
+            effective-path
+            (or domain nil)   ; Pass nil if not specified
+            (or secure nil)   ; Pass nil if not specified
+            (or sameSite nil))))) ; Pass nil if not specified (e.g., "Lax", "Strict", "None")
